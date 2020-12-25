c2ecff671489500a34be3be48ea03f9d3545b7a8
Tihomir Georgiev <georgiev.tm@students.softuni.bg>
2020-12-25 20:39:05 +0100

    Rename a Lesson

---
+++ CSharp-Programming-Basics/HomeWorks-And-Labs/01FirstStepsInCoding/03.DepositCalculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace KalkulatorDepozit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //прочитаме входа
+            double deposit = double.Parse(Console.ReadLine());
+            int depositLenght = int.Parse(Console.ReadLine());
+            double anualInterest = double.Parse(Console.ReadLine());
+
+
+            //изчисляваме натрупаната лихва: 200 * 5.7 % = 11.4лв
+            double acumulatedInterest = (deposit * anualInterest) / 100;
+            //изчисляваме лихвата за 1 месец: 11.4лв./ 12 месеца = 0.95лв
+            double interes = acumulatedInterest / 12;
+
+            //общата сума е 200лв депозит +(3(срок на депозита) * 0.95 лв)
+            double result = deposit + (depositLenght * interes);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ CSharp-Programming-Basics/HomeWorks-And-Labs/01FirstStepsInCoding/06.CharityCampaign/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _6.Blagotvoritelnost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Торта - 45 лв.
+
+            //Гофрета - 5.80 лв.
+
+            //Палачинка – 3.20 лв.
+            
+
+            int days = int.Parse(Console.ReadLine());
+            int bakersCount = int.Parse(Console.ReadLine());
+            int cakesCount = int.Parse(Console.ReadLine());
+            int waffels = int.Parse(Console.ReadLine());
+            int pancakes = int.Parse(Console.ReadLine());
+
+            //Изчисляваме сумата, която се изкарва на ден
+            int cakesProfitForOneDay = cakesCount * 45;
+            double wafflesProfitForOneDay = waffels * 5.80;
+            double pancakesProfitForOneDay = pancakes * 3.20;
+
+            double totalProfitForOneDay = (cakesProfitForOneDay +
+                wafflesProfitForOneDay + pancakesProfitForOneDay) * bakersCount;
+            
+
+
+            //Сума събрана от цялата кампания
+            double totProfitForTheCampaign = totalProfitForOneDay * days;
+
+            //Сума след покриване на разходите 
+            double expences = 1 / 8.0;
+
+            double result = totProfitForTheCampaign - (totProfitForTheCampaign * expences);
+            Console.WriteLine(result);
+
+        }
+    }
+}
