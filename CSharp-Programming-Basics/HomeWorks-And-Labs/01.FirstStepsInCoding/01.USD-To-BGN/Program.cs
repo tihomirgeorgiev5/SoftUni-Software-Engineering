eed9680cf6269294288fc69e1e44208294dee53d
Tihomir Georgiev <georgiev.tm@students.softuni.bg>
2020-12-25 19:05:39 +0100

    Added Programming Basics Exercises

---
+++ CSharp-Programming-Basics/HomeWorks-And-Labs/01.FirstStepsInCoding/01.USD-To-BGN/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace USDKumBGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double test = 1.3455656;
+            Console.WriteLine(Math.Round(test,2));
+
+        }
+    }
+}
