9533425312ba8dadf6e2d2b95dddc7483e9160fe
Tihomir Georgiev <georgiev.tm@students.softuni.bg>
2020-12-26 13:35:30 +0100

    Added Labs in Conditional Statements

---
+++ CSharp-Programming-Basics/HomeWorks-And-Labs/02ConditionalStatements-Lab/06.AreaOfFigures/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace _06._Area_of_Figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figureType = Console.ReadLine();
+            double area = 0;
+            if (figureType == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                area = side * side;
+               
+
+            }
+            else if (figureType == "rectangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                area = a * b;
+                
+            }
+            else if (figureType == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                area = radius * radius * Math.PI;
+               
+
+
+
+            }
+            else if (figureType == "triangle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                area = (side * height) / 2;
+                
+            }
+            Console.WriteLine($"{area:f3}");
+        }
+    }
+}
