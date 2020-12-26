9dc20a8b889ea958a688bbbc60fb1ede0527099f
Tihomir Georgiev <georgiev.tm@students.softuni.bg>
2020-12-26 12:30:59 +0100

    Added Labs in FirstStepsInCoding

---
+++ CSharp-Programming-Basics/HomeWorks-And-Labs/01FirstStepsInCodin-Lab/04.InchesToCentimetres/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InchesToCantimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double inches = double.Parse(Console.ReadLine());
+            double centimeters = inches * 2.54;
+
+            Console.WriteLine(centimeters);
+        }
+
+    }
+}
+    
---
+++ CSharp-Programming-Basics/HomeWorks-And-Labs/01FirstStepsInCodin-Lab/08.PetShop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PetFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Еднa опаковка храна за кучета е на цена 2.50лв;
+            // а всяка останала, която не е за тях струва 4лв;
+            //1. Броят на кучетата - цяло число;
+            //2.Броят на останалите животни - цяло число,
+            //Изхоd На конзолата се отпечатва:{ крайната сума}lv
+
+            double dogFood = 2.5;
+            int otherFood = 4;
+
+            int dog = int.Parse(Console.ReadLine());
+            int other = int.Parse(Console.ReadLine());
+
+            double priceDogFood = dog * dogFood;
+            double priceOtherFood = other * otherFood;
+            double razhodi = priceDogFood + priceOtherFood;
+
+            Console.WriteLine($"{razhodi} lv.");
+
+
+        }
+    }
+}
