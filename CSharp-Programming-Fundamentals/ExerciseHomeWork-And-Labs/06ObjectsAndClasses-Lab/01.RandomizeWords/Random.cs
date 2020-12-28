1e70c95f1e8cc0d2bbbb1a6a67ee01600c9a273b
Tihomir Georgiev <georgiev.tm@students.softuni.bg>
2020-12-28 23:12:25 +0100

    Added Problems in Objects

---
+++ CSharp-Programming-Fundamentals/ExerciseHomeWork-And-Labs/06ObjectsAndClasses-Lab/01.RandomizeWords/Random.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01._Randomize_Words
+{
+    class Random
+    {
+        internal int Next(int v, int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal int Next()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
