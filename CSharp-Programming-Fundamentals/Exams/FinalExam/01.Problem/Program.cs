ab949c52a5b4d8da9144459d1c76ac4b2efcba04
Tihomir Georgiev <georgiev.tm@students.softuni.bg>
2020-12-29 14:30:17 +0100

    Added Problems in FinalExam

---
+++ CSharp-Programming-Fundamentals/Exams/FinalExam/01.Problem/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            string input = Console.ReadLine();
+            string command = Console.ReadLine();
+
+            while (true)
+            {
+                if (command == "Done")
+                {
+                    break;
+                }
+                string[] current = command
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string currentCommand = current[0];
+
+                switch (currentCommand)
+                {
+                    case "Change":
+                        string symbol = current[1];
+                        string replacement = current[2];
+                        for (int i = 0; i < input.Length; i++)
+                        {
+                            input = input.Replace(symbol, replacement);
+                        }
+                        Console.WriteLine(input);
+                       
+                        break;
+                    case "Includes":
+                        string otherString = current[1];
+                        if (input.Contains(otherString))
+                        {
+                            Console.WriteLine("True");
+                        }
+                        else
+                        {
+                            Console.WriteLine("False");
+                        }
+    
+                        break;
+                    case "End":
+                        string end = current[1];
+                        if (input.Length.Equals(end))
+                        {
+                            Console.WriteLine("True");
+                        }
+                        else
+                        {
+                            Console.WriteLine("False");
+                        }
+                        break;
+                    case "Uppercase":
+                        input = input.ToUpper();
+                        Console.WriteLine(input);
+                           
+                        break;
+                    case "FindIndex":
+                        string symbolChar = current[1];
+                        int numberIndex = input.IndexOf(symbolChar);
+                        Console.WriteLine(numberIndex);
+                        break;
+                    case "Cut":
+                        int startIndex = int.Parse(current[1]);
+                        int length = int.Parse(current[2]);
+                        //int total = startIndex + length;
+
+                        input = input.Substring(startIndex, length);
+                        Console.WriteLine(input);
+
+                        
+                        
+                       
+                        break;
+                    default:
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
