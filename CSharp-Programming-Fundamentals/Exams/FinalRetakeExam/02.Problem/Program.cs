3e4885f3f2d709bc37fe9d7883a175e1889ad856
Tihomir Georgiev <georgiev.tm@students.softuni.bg>
2020-12-29 12:17:35 +0100

    Added Problems in Final Exam Retake

---
+++ CSharp-Programming-Fundamentals/Exams/FinalRetakeExam/02.Problem/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int numberOutputs = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOutputs; i++)
+            {
+                string input = Console.ReadLine();
+                string pattern = @"\|([A-Z]{4,})\|:\#([A-Za-z]+ [A-Za-z]+)\#";
+
+                Match match =
+                    Regex.Match(input, pattern);
+
+                if (match.Success)
+                {
+                    string bossName = match.Groups[1].Value;
+                    string title = match.Groups[2].Value;
+                    int lengthName = bossName.Length;
+                    int armour = title.Length;
+
+                    Console.WriteLine($"{bossName}, The {title}");
+                    Console.WriteLine($">> Strength: {lengthName}");
+                    Console.WriteLine($">> Armour: {armour}");
+
+                }
+                else
+                {
+                    Console.WriteLine("Access denied!");
+
+                }
+                
+
+            }
+        }
+    }
+}
---
+++ CSharp-Programming-Fundamentals/Exams/FinalRetakeExam/03.Problem/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> heroes =
+                   new Dictionary<string, List<string>>();
+
+            string input = Console.ReadLine();
+
+            while (true)
+            {
+                if (input == "End")
+                {
+                    break;
+                }
+                string[] command = input.Split(" ",
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                string action = command[0];
+                string heroName = command[1];
+
+                switch (action)
+                {
+                    case "Enroll":
+                        if (!heroes.ContainsKey(heroName))
+                        {
+                            heroes.Add(heroName, new List<string>());
+                            
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{heroName} is already enrolled.");
+                        }
+                        break;
+                    case "Learn":
+                        string spellName = command[2];
+                        if (!heroes.ContainsKey(heroName))
+                        {
+                            Console.WriteLine($"{heroName} doesn't exist.");
+                        }
+                        else
+                        {
+                            if (!heroes[heroName].Contains(spellName))
+                            {
+                                heroes[heroName].Add(spellName);
+                            }
+                            else
+                            {
+                                Console.WriteLine($"{heroName} has already learnt {spellName}.");
+                            }
+                            
+                        }
+                        break;
+                    case "Unlearn":
+                        string spellName2 = command[2];
+                       
+                        if (!heroes.ContainsKey(heroName))
+                        {
+                            Console.WriteLine($"{heroName} doesn't exist.");
+                        }
+                        else if (!heroes[heroName].Contains(spellName2))
+                        {
+                            Console.WriteLine($"{heroName} doesn't know {spellName2}.");
+                        }
+                        else
+                        {
+                            heroes[heroName].Remove(spellName2);
+                        }
+                        
+                       
+                        break;
+                    default:
+                        break;
+                }
+                input = Console.ReadLine();
+
+
+            }
+
+            Console.WriteLine("Heroes:");
+
+            var sortedCollection = heroes
+                .OrderByDescending(s => s.Value.Count)
+                .ThenBy(h => h.Key);
+
+            foreach (var (heroname,spellname) in sortedCollection)
+            {
+                Console.WriteLine($"== {heroname}: {string.Join(", ", spellname)}");
+            }
+        }
+    }
+}
