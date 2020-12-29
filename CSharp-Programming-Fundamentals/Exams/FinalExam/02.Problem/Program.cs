3d876a8783086d5a2b1c43d1bbc184bd1f553d59
Tihomir Georgiev <georgiev.tm@students.softuni.bg>
2020-12-29 14:47:45 +0100

    Added Problem in FinalExam

---
+++ CSharp-Programming-Fundamentals/Exams/FinalExam/02.Problem/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                string pattern = @"\!([A-Za-z]{3,})\!:\[([A-Za-z]{8,})\]";
+
+                Match match = Regex.Match(input, pattern);
+
+                if (match.Success)
+                {
+
+                    string command = match.Groups[1].Value;
+                    string message = match.Groups[2].Value;
+                    int[] result =
+                        message.Select(x => (int)x).ToArray();
+                    Console.WriteLine($"{command}: {string.Join(" ", result)}");
+
+                }
+                else
+                {
+                    Console.WriteLine("The message is invalid");
+                }
+            }
+        }
+    }
+}
