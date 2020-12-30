286d5d6ce319ea9d7e461f369e7e9c5e54ea7f9f
Tihomir Georgiev <georgiev.tm@students.softuni.bg>
2020-12-30 22:29:47 +0100

    Added Problem in FinalExamExercise

---
+++ CSharp-Programming-Fundamentals/MoreExercises/FinalExamExercise/02.TheIsleO/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02_The_Isle_O
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            
+            
+            
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                string pattern = @"([#$%*&])(\w+)\1=(\d+)!!(.+)";
+
+                Match match = Regex.Match(input, pattern);
+
+                if (match.Success)
+                {
+                    string name = match.Groups[2].Value;
+                    int length = int.Parse(match.Groups[3].Value);
+                    string geonashCode = match.Groups[4].Value;
+
+                    if (length == geonashCode.Length)
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        for (int i = 0; i < geonashCode.Length; i++)
+                        {
+                            sb.Append((char)(geonashCode[i] + length));
+                        }
+                        Console.WriteLine($"Coordinates found! {name} -> " +
+                            $"{sb.ToString()}");
+                        break;
+                    }
+                }
+                Console.WriteLine("Nothing found!");
+            }
+
+
+           
+            
+
+            
+        }
+    }
+}
