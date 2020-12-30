7189088afafeb7e6da092065843f9598f734f014
Tihomir Georgiev <georgiev.tm@students.softuni.bg>
2020-12-30 22:27:17 +0100

    Added Problem in FinalExamExercise

---
+++ CSharp-Programming-Fundamentals/MoreExercises/FinalExamExercise/02.Registration/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02_Registration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int countSuccess = 0;
+            string pattern =
+                @"U\$([A-Z][a-z]{3,})U\$P\@\$([a-z]{5,}\d+)P\@\$";
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                Match match = Regex.Match(input, pattern);
+
+                if (match.Success)
+                {
+                    countSuccess++;
+                    string username = match.Groups[1].Value;
+                    string password = match.Groups[2].Value;
+
+                    Console.WriteLine("Registration was successful");
+                    Console.WriteLine($"Username:{username}, Password:{password}");
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Invalid username or password");
+                }
+            }
+            Console.WriteLine($"Successful registrations: {countSuccess}");
+
+        }
+    }
+}
