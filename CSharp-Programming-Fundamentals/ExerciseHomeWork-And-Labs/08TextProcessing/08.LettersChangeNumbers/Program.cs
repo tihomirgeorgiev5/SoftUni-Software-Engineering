78c67b2012e18c7b2d9c5cadddf7895897c58d83
Tihomir Georgiev <georgiev.tm@students.softuni.bg>
2021-01-01 22:56:55 +0100

    Added Problem in TextProcessing

---
+++ CSharp-Programming-Fundamentals/ExerciseHomeWork-And-Labs/08TextProcessing/08.LettersChangeNumbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Letters_Change_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            double result = 0;
+           
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string temp = input[i];
+                char firstLetter = temp[0];
+                char lastLetter = temp[temp.Length - 1];
+
+                double number = double.Parse(temp.Substring(1, temp.Length - 2));
+
+                int firstElementIndex = alpha.IndexOf(char.ToUpper(firstLetter));
+                int secondElementIndex = alpha.IndexOf(char.ToUpper(lastLetter));
+
+                if ((int)firstLetter >= 65 && (int)firstLetter <= 90)
+                {
+                    number /= firstElementIndex + 1;
+                }
+                else
+                {
+                    number *= firstElementIndex + 1;
+                }
+
+                if ((int) lastLetter >= 65 && (int)lastLetter <= 90)
+                {
+                    number -= secondElementIndex + 1;
+                }
+                else
+                {
+                    number += secondElementIndex + 1;
+                }
+                result += number;
+
+
+            }
+            Console.WriteLine($"{result :f2}");
+        }
+    }
+}
