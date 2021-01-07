5832b0e2f0cb9c95ec2f01d46fdba6c458faa2ed
Tihomir Georgiev <georgiev.tm@students.softuni.bg>
2021-01-07 20:48:53 +0100

    Added Problem in Methods-Lab

---
+++ CSharp-Programming-Fundamentals/ExerciseHomeWork-And-Labs/04Methods-Lab/05.Orders/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _05._Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantityProduct = int.Parse(Console.ReadLine());
+            CalculateTotalPrice(product, quantityProduct);
+
+            static void CalculateTotalPrice(string name, int count)
+            {
+                double price = 0;
+                switch (name)
+                {
+                    case "coffee":
+                        price = 1.50 * count;
+                        break;
+                    case "water":
+                        price = 1.00 * count;
+                        break;
+                    case "coke":
+                        price = 1.40 * count;
+                        break;
+                    case "snacks":
+                        price = 2.00 * count;
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine($"{price:f2}");
+
+
+            }
+        }
+    }
+}
