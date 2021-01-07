4d575416988a3b7e27f674c8d51de1d52b53a822
Tihomir Georgiev <georgiev.tm@students.softuni.bg>
2021-01-07 21:30:39 +0100

    Added Problem in Methods-Lab

---
+++ CSharp-Programming-Fundamentals/ExerciseHomeWork-And-Labs/04Methods-Lab/06.CalculateRectangleArea/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _06._Calculate_Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = int.Parse(Console.ReadLine());
+            double b = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine(CalculateRectArea(a, b));
+
+        }
+        static double CalculateRectArea(double a, double b)
+        {
+            return a * b;
+        }
+
+    }
+}
