c0f2d91fa866fe8a2b53b8d650057fa634894fb1
Tihomir Georgiev <georgiev.tm@students.softuni.bg>
2021-01-08 19:37:21 +0100

    Added Exercise in MoreExercises

---
+++ CSharp-Programming-Fundamentals/MoreExercises/Methods/01.Exercises/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01.Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(SmallestInteger(num1, num2, num3));
+            
+
+            
+        }
+        static int SmallestInteger(int a, int b, int c)
+        {
+
+            return Math.Min(Math.Min(a, b), c);
+            
+        }
+    }
+}
