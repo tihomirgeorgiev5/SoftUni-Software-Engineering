6d8b92ff301d041abd0463f8cd7d6b6d5d290c64
Tihomir Georgiev <georgiev.tm@students.softuni.bg>
2021-01-08 19:39:17 +0100

    Added Exercise in MoreExercise

---
+++ CSharp-Programming-Fundamentals/MoreExercises/Methods/02.Exercises/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02.Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            VowelsCount(input);
+        }
+        static void VowelsCount(string word)
+        {
+            string newText = word.ToLower();
+            int counter = 0;
+            for (int i = 0; i < newText.Length; i++)
+            {
+                if ((int)newText[i] == 97 || (int)newText[i] == 101 || (int)newText[i] == 105 
+                    || (int)newText[i] == 111 || (int)newText[i] == 117 || (int)newText[i] == 121)
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
