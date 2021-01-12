51cc679fee8bd21758c70c8c16c3a6b46b15eab2
Tihomir Georgiev <georgiev.tm@students.softuni.bg>
2021-01-12 21:52:46 +0100

    Added Problem in Stacks

---
+++ CSharp-Advanced/HomeWorks-And-Labs/01-StacksAndQueues-Lab/01.ReverseStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _1._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> reversedStack = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                reversedStack.Push(input[i]);
+            }
+            while (reversedStack.Count > 0)
+            {
+                Console.Write(reversedStack.Pop());
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
