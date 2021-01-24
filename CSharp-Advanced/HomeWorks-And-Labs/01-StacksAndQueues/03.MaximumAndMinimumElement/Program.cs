ffd94adfda91f7df24f808bde92a0f33f31c3788
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-01-24 18:23:03 +0100

    Add Exercise in Stacks

---
+++ CSharp-Advanced/HomeWorks-And-Labs/01-StacksAndQueues/03.MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Maximum_and_Minimum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Stack<int> numbers = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                .Split(" ").ToArray();
+
+                
+
+                string command = input[0];
+                
+                switch (command.ToString())
+                {
+                    case "1":
+                        int pushedEllement = int.Parse(input[1]);
+                        numbers.Push(pushedEllement);
+                        break;
+                    case "2":
+                        if (numbers.Any())
+                        {
+                            numbers.Pop();
+                        }
+
+                        break;
+                    case "3":
+                        if (numbers.Any())
+                        {
+                            Console.WriteLine(numbers.Max());
+                        }
+
+                        break;
+                    case "4":
+                        if (numbers.Any())
+                        {
+                            Console.WriteLine(numbers.Min());
+                        }
+
+                        break;
+
+                    default:
+                        break;
+                }
+            
+                
+            }
+            
+
+            
+
+            Console.WriteLine(string.Join(", ", numbers));
+            
+        }
+    }
+}
