9015b30ae167f2575dc5dd9d1770a81ab96f1a42
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-01-25 20:56:16 +0100

    Added Exercise to Stack

---
+++ CSharp-Advanced/HomeWorks-And-Labs/01-StacksAndQueues/05.FashionBoutique/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Fashion_Boutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothesInBox = Console.ReadLine()
+                .Split(" ").Select(int.Parse).ToArray();
+
+            int capacityRack = int.Parse(Console.ReadLine());
+
+            Stack<int> allClothes = new Stack<int>(clothesInBox);
+            int currentCapacityRack = capacityRack;
+            int racksCount = 1;
+
+            
+
+            while (allClothes.Any())
+            {
+                int clothes = allClothes.Pop();
+                currentCapacityRack -= clothes;
+
+                if (currentCapacityRack < 0)
+                {
+                    racksCount++;
+                    currentCapacityRack = capacityRack - clothes;
+                }
+            }
+
+            Console.WriteLine(racksCount);
+
+        }
+    }
+}
