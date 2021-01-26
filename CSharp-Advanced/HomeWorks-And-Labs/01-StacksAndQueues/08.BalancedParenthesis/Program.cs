1aac6f2754b294f99f6c999d18eee8dc8167f9a9
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-01-26 23:43:14 +0100

    Added Exercise in Stacks

---
+++ CSharp-Advanced/HomeWorks-And-Labs/01-StacksAndQueues/08.BalancedParenthesis/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Balanced_Parenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> open = new Stack<char>();
+
+            string input = Console.ReadLine();
+            bool isValid = true;
+
+            foreach (char item in input)
+            {
+                if (item == '(' || item == '{' || item == '[')
+                {
+                    open.Push(item);
+                }
+                else
+                {
+                    bool isFirstValid = item == ')' && open.Pop() == '(';
+                    bool isSecondValid = item == '{' && open.Pop() == '}';
+                    bool isThirdtValid = item == ']' && open.Pop() == '[';
+
+                    if (!isFirstValid && !isSecondValid && !isThirdtValid)
+                    {
+                        isValid = false;
+                        Console.WriteLine("NO");
+                        break;
+                    }
+
+                }
+            }
+            if (!open.Any())
+            {
+                Console.WriteLine("YES");
+            }
+            else if(isValid == false)
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
