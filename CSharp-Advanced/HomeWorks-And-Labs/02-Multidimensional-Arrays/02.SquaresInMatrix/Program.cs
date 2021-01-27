04849b9afc928b839a1cefa0a95e828044202c19
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-01-27 23:29:31 +0100

    Added Exercise in Matrix

---
+++ CSharp-Advanced/HomeWorks-And-Labs/02-Multidimensional-Arrays/02.SquaresInMatrix/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            int rows = input[0];
+            int cols = input[1];
+
+            char[,] matrix = new char[rows, cols];
+
+            fillMatrix(matrix);
+            int count = 0;
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    char currentEllement = matrix[row, col];
+                    if (currentEllement == matrix[row, col + 1]
+                        && currentEllement == matrix[row + 1, col]
+                        && currentEllement == matrix[row + 1, col + 1])
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
+
+
+        }
+
+        private static void fillMatrix(char[,] numbers)
+        {
+            for (int row = 0; row < numbers.GetLength(0); row++)
+            {
+                char[] currentRow = Console.ReadLine()
+                    .Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(char.Parse)
+                    .ToArray();
+                for (int col = 0; col < numbers.GetLength(1); col++)
+                {
+                    numbers[row, col] = currentRow[col];
+                }
+            }
+        }
+    }
+}
