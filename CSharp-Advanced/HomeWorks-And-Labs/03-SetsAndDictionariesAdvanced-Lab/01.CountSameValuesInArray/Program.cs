491a8c776696afdf93fe5adb5a0dbe0cc83dc34e
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-01-31 15:47:08 +0100

    Added Exercise in Maps-Lab

---
+++ CSharp-Advanced/HomeWorks-And-Labs/03-SetsAndDictionariesAdvanced-Lab/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+
+namespace _01._Count_Same_Values_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-GB");
+            double[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, int> times = new Dictionary<double, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!times.ContainsKey(input[i]))
+                {
+                    times.Add(input[i], 0);
+                }
+                times[input[i]]++;
+            }
+
+            foreach (var time in times)
+            {
+                Console.WriteLine($"{time.Key} - {time.Value} times");
+            }
+        }
+    }
+}
