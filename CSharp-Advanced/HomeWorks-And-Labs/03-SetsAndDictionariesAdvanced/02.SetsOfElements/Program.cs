2d44512328ba996b513c7d09190ee237d4f85c04
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-02 22:49:11 +0100

    Added Exercise in Maps

---
+++ CSharp-Advanced/HomeWorks-And-Labs/03-SetsAndDictionariesAdvanced/02.SetsOfElements/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ")
+                .Select(int.Parse).ToArray();
+
+            HashSet<int> num = new HashSet<int>();
+            HashSet<int> num2 = new HashSet<int>();
+
+            int set1Count = input[0];
+            int set2Count = input[1];
+
+
+            for (int i = 0; i < set1Count; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                num.Add(currentNum);
+            }
+
+            for (int i = 0; i < set2Count; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                num2.Add(currentNum);
+            }
+
+            var intersection = num.Intersect(num2);
+
+            foreach (var item in intersection)
+            {
+                Console.Write($"{item} ");
+            }
+
+          
+        }
+    }
+}
