e00bd66794d641c4a665db90fd4984986a932b79
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-02 23:45:13 +0100

    Added Exercise in Maps

---
+++ CSharp-Advanced/HomeWorks-And-Labs/03-SetsAndDictionariesAdvanced/04.EvenTimes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+
+                if (!dict.ContainsKey(currentNum))
+                {
+                    dict.Add(currentNum, 0);
+                }
+
+                dict[currentNum]++;
+            }
+
+            foreach (var item in dict)
+            {
+                if (item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                    break;
+                }
+            }
+        }
+    }
+}
