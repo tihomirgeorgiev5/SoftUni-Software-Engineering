ac3c213c196bc2d5f62473407a8256ba31c365c7
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-02 23:14:48 +0100

    Added Exercise in Maps

---
+++ CSharp-Advanced/HomeWorks-And-Labs/03-SetsAndDictionariesAdvanced/03.PeriodicTable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> hashSet = new HashSet<string>();
+
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+
+                foreach (var item in input)
+                {
+                    hashSet.Add(item);
+                }
+
+            }
+            var sorted = hashSet.OrderBy(h => h);
+            Console.WriteLine(string.Join(" ",sorted));
+        }
+    }
+}
