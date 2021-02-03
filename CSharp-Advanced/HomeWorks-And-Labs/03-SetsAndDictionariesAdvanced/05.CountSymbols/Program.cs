dd0a625463c356eda7a453ece6cfae31a184dcb9
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-03 21:18:21 +0100

    Added Exercise in Maps

---
+++ CSharp-Advanced/HomeWorks-And-Labs/03-SetsAndDictionariesAdvanced/05.CountSymbols/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> dict =
+                new SortedDictionary<char, int>();
+
+           string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+
+                if (!dict.ContainsKey(currentChar))
+                {
+                    dict.Add(currentChar, 0);
+                }
+                dict[currentChar]++;
+            }
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
