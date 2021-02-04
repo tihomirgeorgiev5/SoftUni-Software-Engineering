b5ac556eada9b46a22a9e4947ec58c18509fc3fc
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-04 20:37:11 +0100

    Added Exercise in Func-Lab

---
+++ CSharp-Advanced/HomeWorks-And-Labs/05-FunctionalProgramming-Lab/02.SumNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintSumAndCount(
+                int.Parse,
+                a => a.Length,
+                array => array.Sum());
+        }
+
+        static void PrintSumAndCount(Func<string, int> parser,
+            Func<int[], int> countGetter,
+            Func<int[], int> sumCalculator)
+        {
+            int[] array = Console.ReadLine()
+                 .Split(", ")
+                 .Select(parser)
+                 .ToArray();
+
+            Console.WriteLine(countGetter(array));
+            Console.WriteLine(sumCalculator(array));
+        }
+    }
+}
