5c43489a24baa5cc5edba9e826e406cd4e573d0c
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-06 13:31:46 +0100

    Added Exercise in Action

---
+++ CSharp-Advanced/HomeWorks-And-Labs/05-FunctionalProgramming/01.ActionPrint/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Action_Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            List<string> newLine = names.Select(name => $"{name}").ToList();
+
+            Action<List<string>> printNames =
+                n => Console.WriteLine(string.Join(Environment.NewLine, n));
+
+            printNames(newLine);
+        }
+    }
+
+}
