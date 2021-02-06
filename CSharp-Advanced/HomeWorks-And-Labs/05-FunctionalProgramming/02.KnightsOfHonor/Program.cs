7fcb1379e708086f936e07dc0bfffbe837f2f45e
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-06 13:23:39 +0100

    Added Exercise in Func

---
+++ CSharp-Advanced/HomeWorks-And-Labs/05-FunctionalProgramming/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            List<string> knights = names.
+                Select(name => $"Sir {name}").ToList();
+
+            Action<List<string>> printNames =
+                n => Console.WriteLine(string.Join(Environment.NewLine, n));
+            printNames(knights);
+
+        }
+        static void PrintNamesMethod(List<string> names)
+        {
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+        }
+    }
+}
