e911bcd487e48e98b72a36ec24ffecc32b198149
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-06 14:04:35 +0100

    Added Exercise in Func

---
+++ CSharp-Advanced/HomeWorks-And-Labs/05-FunctionalProgramming/03.CustomMinFunction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            Func<List<int>, int> minNumber =
+                numbers =>
+                {
+                    int minNum = int.MaxValue;
+
+                    foreach (var num in numbers)
+                    {
+                        if (num < minNum)
+                        {
+                            minNum = num;
+                        }
+                    }
+
+                    return minNum;
+                };
+
+            int searchedMinNum = minNumber(numbers);
+            Console.WriteLine(searchedMinNum);
+        }
+    }
+}
