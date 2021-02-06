370570666087718412087790dd50e07d175e91f7
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-06 18:39:23 +0100

    Added Exercise in Func

---
+++ CSharp-Advanced/HomeWorks-And-Labs/05-FunctionalProgramming/05.AppliedArithmetics/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string command = Console.ReadLine();
+
+            Func<int, int> mathFunctions = num => num;
+            Action<List<int>> print =
+                nums => Console.WriteLine(string.Join(" ",nums));
+
+            while (command != "end")
+            {
+                if (command == "add")
+                {
+                    mathFunctions = num => num + 1;
+                    numbers = numbers.Select(mathFunctions).ToList();
+                }
+                else if (command == "multiply")
+                {
+                    mathFunctions = num => num * 2;
+                    numbers = numbers.Select(mathFunctions).ToList();
+                }
+                else if (command == "subtract")
+                {
+                    mathFunctions = num => num - 1;
+                    numbers = numbers.Select(mathFunctions).ToList();
+                }
+                else if (command == "print")
+                {
+                    print(numbers);
+                }
+
+                command = Console.ReadLine();
+            }
+
+        }
+    }
+}
