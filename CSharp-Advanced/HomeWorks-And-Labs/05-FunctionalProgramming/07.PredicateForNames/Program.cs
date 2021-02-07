c21f41f2e84f16f4036d873d1a3f16ebf7deb534
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-07 12:08:49 +0100

    Added Exercise in Func

---
+++ CSharp-Advanced/HomeWorks-And-Labs/05-FunctionalProgramming/07.PredicateForNames/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Predicate_For_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Func<string, int, bool> func =
+                (name, length) => name.Length <= length;
+
+            names = names.Where(name => func(name, n)).ToList();
+
+            Action<List<string>> print = names =>
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+
+            print(names);
+
+
+
+        }
+    }
+}
