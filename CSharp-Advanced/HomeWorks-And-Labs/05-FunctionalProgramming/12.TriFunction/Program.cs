8093e8b4e8c91fa51760d576bef50d9e4a6fc87d
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-07 15:47:37 +0100

    Added Exercise in Func

---
+++ CSharp-Advanced/HomeWorks-And-Labs/05-FunctionalProgramming/12.TriFunction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12._TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> people = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Func<string, int> getAsciiSum = p => p.Select(c => (int)c).Sum();
+
+            Func<List<string>, int, Func<string, int>, string> nameFunc = (people, n, func) => people.FirstOrDefault(p => func(p) >= n);
+
+            string res = nameFunc(people, n, getAsciiSum);
+
+            Console.WriteLine(res);
+        }
+    }
+}
