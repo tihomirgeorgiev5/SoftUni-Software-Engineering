42a2f660af2cbde01f5d1ff132930089c5c31461
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-07 13:41:03 +0100

    Added Exercise in Func

---
+++ CSharp-Advanced/HomeWorks-And-Labs/05-FunctionalProgramming/09.ListOfPredicates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._List_Of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int end = int.Parse(Console.ReadLine());
+
+            List<int> dividers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            Func<int, int, bool> predicate =
+                (num, d) => num % d == 0;
+
+            for (int i = 1; i <= end ; i++)
+            {
+                if (dividers.All(d => predicate(i, d)))
+                {
+                    Console.Write(i + " ");
+                }
+
+            }
+
+
+        }
+    }
+}
