c82a30e4667883c78db5807048d6e3f473945798
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-07 15:14:09 +0100

    Added Exercise in Func

---
+++ CSharp-Advanced/HomeWorks-And-Labs/05-FunctionalProgramming/11.ThePartyReservationFilterModule/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11._The_Party_Reservation_Filter_Module
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> people = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string command = Console.ReadLine();
+            List<string> filters = new List<string>();
+
+            while (command != "Print")
+            {
+                string[] tokens = command.Split(";");
+                string commandName = tokens[0];
+                string filterType = tokens[1];
+                string argument = tokens[2];
+
+                if (commandName == "Add filter")
+                {
+                    filters.Add($"{filterType};{argument}");
+                }
+                else if (commandName == "Remove filter")
+                {
+                    filters.Remove($"{filterType};{argument}");
+                }
+
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var filterLine in filters)
+            {
+                string[] tokens = filterLine.Split(";");
+                string filterType = tokens[0];
+                string argument = tokens[1];
+
+                switch (filterType)
+                {
+                    case "Starts with":
+                        people = people.Where(p => !p.StartsWith(argument)).ToList();
+                        break;
+                    case "Ends with":
+                        people = people.Where(p => !p.EndsWith(argument)).ToList();
+                        break;
+                    case "Length":
+                        people = people.Where(p => p.Length != int.Parse(argument)).ToList();
+                        break;
+                    case "Contains":
+                        people = people.Where(p => !p.Contains(argument)).ToList();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ",people));
+
+        }
+    }
+}
