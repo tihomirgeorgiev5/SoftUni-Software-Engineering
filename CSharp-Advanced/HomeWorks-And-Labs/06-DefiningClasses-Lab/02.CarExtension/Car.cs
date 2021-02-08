88a8569833e07652d7a72f5dc274748635be7404
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-08 21:13:16 +0100

    Added Exercise in Classes-Lab

---
+++ CSharp-Advanced/HomeWorks-And-Labs/06-DefiningClasses-Lab/02.CarExtension/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public double FuelQuantity { get; set; }
+
+        public double FuelConsumption { get; set; }
+
+        public void Drive(double distance)
+        {
+            double fuelToConsum = distance * FuelConsumption;
+
+            if (FuelQuantity - fuelToConsum >= 0)
+            {
+                FuelQuantity -= fuelToConsum;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI ()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}L";
+        }
+    }
+}
---
+++ CSharp-Advanced/HomeWorks-And-Labs/06-DefiningClasses-Lab/02.CarExtension/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "VW";
+            car.Model = "Golf";
+            car.Year = 2025;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 10;
+
+            car.Drive(2000);
+
+            Console.WriteLine(car.WhoAmI());                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+        }
+    }
+}
