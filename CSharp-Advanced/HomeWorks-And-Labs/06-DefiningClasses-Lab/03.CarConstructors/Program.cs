a9edf67a74029287d3a0437753da2434271b2893
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-09 18:33:19 +0100

    Added Exercise in Classes-Lab

---
+++ CSharp-Advanced/HomeWorks-And-Labs/06-DefiningClasses-Lab/03.CarConstructors/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car bmw = new Car("BMW", "X6", 1993, 5003, -50);
+            Car defaultGolf = new Car();
+            Car car = new Car();
+            car.Make = "VW";
+            car.Model = "Golf";
+            car.Year = 2025;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 10;
+
+            car.Drive(2000);
+
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
