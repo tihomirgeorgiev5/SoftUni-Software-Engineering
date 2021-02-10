af8f1a8e50741149983cd9100d616a296a43949f
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-10 19:48:39 +0100

    Added Exercise in Classes-Exercises

---
+++ CSharp-Advanced/HomeWorks-And-Labs/06-DefiningClasses/02.CreatingConstructors/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person person1 = new Person();
+
+            Console.WriteLine($"{person1.Name}=>{person1.Age}");
+
+            Person person2 = new Person(12);
+            Console.WriteLine($"{person2.Name}=>{person2.Age}");
+
+            Person person3 = new Person("Pesho", 23);
+            Console.WriteLine($"{person3.Name}=>{person3.Age}");
+        }
+    }
+}
