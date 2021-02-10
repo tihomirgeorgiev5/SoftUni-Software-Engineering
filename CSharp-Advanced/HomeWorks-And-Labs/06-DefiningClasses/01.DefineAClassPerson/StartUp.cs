ae47ff156d0e517b549d3cf9cf7179ed08b4b4ba
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-10 18:54:50 +0100

    Added Exercise in Classes-Exercises

---
+++ CSharp-Advanced/HomeWorks-And-Labs/06-DefiningClasses/01.DefineAClassPerson/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new Person("Pesho", 45);
+            Console.WriteLine($"{person.Name} => {person.Age}");
+        }
+    }
+}
