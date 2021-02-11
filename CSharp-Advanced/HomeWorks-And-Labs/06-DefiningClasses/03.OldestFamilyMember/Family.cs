117cf498f18c3f6d7b5c18b8e8b8b66493d6885d
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-12 00:31:07 +0100

    Added Exercise in Classes

---
+++ CSharp-Advanced/HomeWorks-And-Labs/06-DefiningClasses/03.OldestFamilyMember/Family.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OldestFamilyMember
+{
+    public class Family
+    {
+        public Family()
+        {
+            People = new List<Person>();
+        }
+        public List<Person> People { get; set; }
+
+        public void AddMember(Person member)
+        {
+            People.Add(member);
+        }
+
+        public Person GetOldestPerson()
+        {
+            int maxAge = int.MinValue;
+            Person person = null;
+
+            foreach (var currentPerson in People)
+            {
+                var currentAge = currentPerson.Age;
+
+                if (currentAge > maxAge)
+                {
+                    maxAge = currentAge;
+                    person = currentPerson;
+                }
+            }
+            return person;
+
+           
+
+
+        }
+    }
+}
