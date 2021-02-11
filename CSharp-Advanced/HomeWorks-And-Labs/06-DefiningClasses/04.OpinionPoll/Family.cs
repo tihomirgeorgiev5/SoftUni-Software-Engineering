405a956a2b5bfb05a485b801f4dbacae53324507
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-12 00:21:24 +0100

    Added Exercise in Classes

---
+++ CSharp-Advanced/HomeWorks-And-Labs/06-DefiningClasses/04.OpinionPoll/Family.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OpinionPoll
+{
+    public class Family
+    {
+        public Family()
+        {
+            People = new List<Person>();
+        }
+        public List<Person> People { get; set; }
+
+        public void AddMember(Person member)
+        {
+            People.Add(member);
+        }
+
+        public Person GetOldestPerson()
+        {
+            return People.OrderByDescending(x => x.Age).First();
+        }
+
+        public Person[] GetPeople()
+        {
+            var people = People.Where(x => x.Age > 30).OrderBy(x => x.Name).ToArray();
+
+            return people;
+        }
+    }
+}
