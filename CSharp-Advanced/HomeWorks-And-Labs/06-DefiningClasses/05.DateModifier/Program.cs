6241be689372f4617c39950d3de53c9804dd198a
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-11 23:57:04 +0100

    Added Exercise in Classes

---
+++ CSharp-Advanced/HomeWorks-And-Labs/06-DefiningClasses/05.DateModifier/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DateModifierExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string startDate = Console.ReadLine();
+            string endDate = Console.ReadLine();
+
+            DateModifier dateModifier = new DateModifier();
+
+            var result = dateModifier.GetDaysDifference(startDate, endDate);
+
+            Console.WriteLine(result);
+        }
+    }
+}
