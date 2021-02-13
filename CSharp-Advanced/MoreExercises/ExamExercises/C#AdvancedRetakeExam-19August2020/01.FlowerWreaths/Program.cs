59bc590f63336429969c027d66ade6ad5f5ac276
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-13 17:27:52 +0100

    Added Exam in ExamExercises

---
+++ CSharp-Advanced/MoreExercises/ExamExercises/C#AdvancedRetakeExam-19August2020/01.FlowerWreaths/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Flower_Wreaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> lilies = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+
+            Queue<int> roses = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+
+            int wreaths = 0;
+            int flowers = 0;
+
+            while (roses.Count > 0 && lilies.Count > 0)
+            {
+                int lilie = lilies.Pop();
+                int rose = roses.Dequeue();
+                int sum = lilie + rose;
+
+                while (true)
+                {
+                   
+                    if (sum == 15)
+                    {
+                        wreaths++;
+                        break;
+                    }
+                    else if (sum < 15)
+                    {
+                        flowers += sum;
+                        break;
+                    }
+                    else
+                    {
+                        sum -= 2;
+                        
+                    }
+
+                }
+            }
+
+            wreaths += flowers / 15;
+
+            if (wreaths >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreaths} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5-wreaths} wreaths more!");
+            }
+
+
+                
+
+
+            
+        }
+    }
+}
