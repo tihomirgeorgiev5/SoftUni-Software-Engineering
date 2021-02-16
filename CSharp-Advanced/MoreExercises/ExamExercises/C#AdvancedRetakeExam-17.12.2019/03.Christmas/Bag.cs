87704535fd0200c4d5b8079b64e1b29d8c7e4284
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-16 21:42:53 +0100

    Added Exam in ExamExercises

---
+++ CSharp-Advanced/MoreExercises/ExamExercises/C#AdvancedRetakeExam-17.12.2019/03.Christmas/Bag.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Christmas
+{
+    public class Bag
+    {
+        private List<Present> data;
+        public Bag(string color, int capacity)
+        {
+            Color = color;
+            Capacity = capacity;
+            data = new List<Present>();
+        }
+
+        public string Color { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int Count { get { return data.Count; } }
+
+        public void Add(Present present)
+        {
+            if (Count < Capacity)
+            {
+                data.Add(present);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Present presentToRemove = data.FirstOrDefault(p => p.Name == name);
+            if (presentToRemove != null)
+            {
+                data.Remove(presentToRemove);
+                return true;
+            }
+            return false;
+        }
+
+        public Present GetHeaviestPresent()
+        {
+            Present heaviestPresent = data.OrderByDescending(p => p.Weight).FirstOrDefault();
+            return heaviestPresent;
+        }
+
+        public Present GetPresent(string name)
+        {
+            Present present = data.FirstOrDefault(p => p.Name == name);
+            return present;
+        }
+
+        public string Report()
+        {
+            StringBuilder report = new StringBuilder();
+            report.AppendLine($"{Color} bag contains:");
+
+            foreach (var present in data)
+            {
+                report.AppendLine(present.ToString());
+            }
+            return report.ToString().TrimEnd();
+        }
+
+
+
+
+
+    }
+}
---
+++ CSharp-Advanced/MoreExercises/ExamExercises/C#AdvancedRetakeExam-17.12.2019/03.Christmas/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Christmas
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Present one = new Present("Doll", 100, "girl");
+            Present two = new Present("Truck", 200, "boy");
+
+            Bag blueBag = new Bag("blue", 5);
+
+            blueBag.Add(one);
+            blueBag.Add(two);
+
+            Console.WriteLine(blueBag.GetPresent("Truck").ToString());
+        }
+    }
+}
