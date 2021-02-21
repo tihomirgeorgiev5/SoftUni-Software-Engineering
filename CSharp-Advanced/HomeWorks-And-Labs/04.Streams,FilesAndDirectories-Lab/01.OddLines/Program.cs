9e6fa9d2ed80315f42480b950519fa2f9dcf00df
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-21 22:54:37 +0100

    Added Exercise in Streams-Lab

---
+++ CSharp-Advanced/HomeWorks-And-Labs/04.Streams,FilesAndDirectories-Lab/01.OddLines/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace _01.Odd_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader(@"C:\Users\43650\source\repos\STREAMS, FILES AND DIRECTORIES\01.Odd Lines\input.txt")) 
+            {
+                string currentRow = reader.ReadLine();
+                int row = 0;
+                using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+                {
+                    while(currentRow != null)
+                    {
+                        if (row % 2 != 0)
+                        {
+                            writer.WriteLine(currentRow);
+                        }
+
+                        currentRow = reader.ReadLine();
+                        row++;
+                    }
+                }
+                    
+                
+            }
+
+            //string line = reader.ReadLine();
+            //
+            //Console.WriteLine(line);
+            //
+            //reader.Close();
+        }   // 
+    }
+}
