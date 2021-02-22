e38588e09f1d59202b0ec6a40dfb4743cc402023
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-22 22:38:11 +0100

    Add Exercise in Streams-Lab

---
+++ CSharp-Advanced/HomeWorks-And-Labs/04.Streams,FilesAndDirectories-Lab/04.RecursiveDirectories/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace _04.RecursiveDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = Console.ReadLine();
+
+            var files = Directory.GetFiles(folderPath);
+
+            long fileSize = 0;
+
+            foreach (var filePath in files)
+            {
+                FileInfo file = new FileInfo(filePath);
+
+                Console.WriteLine(file.FullName);
+                Console.WriteLine(file.Length);
+                Console.WriteLine(file.Extension);
+
+                fileSize += file.Length;
+            }
+
+            Console.WriteLine(fileSize / 1024.0);
+        }
+    }
+}
