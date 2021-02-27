af7668a4da5eb37b8c8761263067fcba9cac869f
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-28 00:47:18 +0100

    Added Exercise in Inheritance

---
+++ CSharp-OOP/HomeWorks-And-Labs/01-Inheritance/06.Animals(waitingJudge)/Kitten.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Kitten : Cat
+    {
+        private const string DefaulGender = "female";
+        public Kitten(string name, int age) : base(name, age, DefaulGender)
+        {
+        }
+
+        public override string ProduceSound()
+        {
+            return "Meow";
+        }
+    }
+}
