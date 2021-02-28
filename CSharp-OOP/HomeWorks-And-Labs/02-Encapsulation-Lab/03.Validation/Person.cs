91ca704b8ff6c1cbb8d8624dbe4e47fd44b8661b
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-28 17:34:13 +0100

    Added Exercise in Encapsulation-Lab

---
+++ CSharp-OOP/HomeWorks-And-Labs/02-Encapsulation-Lab/03.Validation/Person.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("ERRRORRRR");
+                }
+                this.firstName = value;
+            }
+        }
+
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("ERRRORRRR");
+                }
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("ERRRORRRR");
+                }
+                this.age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            { 
+                return this.salary;
+            }
+            private set
+            {
+                if (salary < 460)
+                {
+                    throw new ArgumentException("ERRRORRRR");
+                }
+                this.salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age < 30)
+            {
+                percentage /= 2;
+            }
+
+            Salary = Salary + Salary * percentage / 100;
+        }
+  
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {Salary:f2} leva.";
+        }
+    }
+}
