8ca6681a07dfff1609e35b24121657a3dab6e7c8
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-02-28 22:31:04 +0100

    Added Exercise in Encapsulation-Lab

---
+++ CSharp-OOP/HomeWorks-And-Labs/02-Encapsulation-Lab/04.Team/Person.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            private set
+            {
+                if (value.Length > 3)
+                {
+                    this.firstName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            private set
+            {
+                if (value.Length > 3)
+                {
+                    this.lastName = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            private set
+            {
+                if (value > 0)
+                {
+                    this.age = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+            }
+        }
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+            private set
+            {
+                if (value > 460)
+                {
+                    this.salary = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age < 30)
+            {
+                Salary += Salary * percentage / 200;
+            }
+            else
+
+                Salary += Salary * percentage / 100;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+    }
+    
+}
+
---
+++ CSharp-OOP/HomeWorks-And-Labs/02-Encapsulation-Lab/04.Team/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-GB");
+            Person personBubi = new Person("Bubi", "Bubov", 15, 460);
+            try
+            {
+                var lines = int.Parse(Console.ReadLine());
+                var persons = new List<Person>();
+                for (int i = 0; i < lines; i++)
+                {
+                    var cmdArgs = Console.ReadLine().Split();
+                    var person = new Person(cmdArgs[0],
+                                            cmdArgs[1],
+                                            int.Parse(cmdArgs[2]),
+                                            decimal.Parse(cmdArgs[3]));
+
+                    persons.Add(person);
+                }
+               
+
+                Team team = new Team("dreamTeam");
+
+                persons.ForEach(p => team.AddPlayer(p));
+
+                Console.WriteLine($"First Team: {team.FirstTeam.Count}");
+                Console.WriteLine($"Reserve Team: {team.ReserveTeam.Count}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                
+            }
+            
+        }
+    }
+}
