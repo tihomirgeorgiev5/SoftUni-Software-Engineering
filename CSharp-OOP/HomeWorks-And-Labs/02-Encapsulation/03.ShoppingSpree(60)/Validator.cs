ca3ff1da4f41304d8c3b68ab683c05c8d948a188
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-03-06 01:17:36 +0100

    Added Exercise in -encapsulation

---
+++ CSharp-OOP/HomeWorks-And-Labs/02-Encapsulation/03.ShoppingSpree(60)/Validator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public static class Validator
+    {
+        public static void ThrowIfStringIsNullOfEmpty (string str, string exceptionMessage)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                throw new ArgumentException(exceptionMessage);
+            }
+        }
+
+        public static void ThrowIfNumberIsNegative (decimal number, string exceptionMessage)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException(exceptionMessage);
+            }
+        }
+    }
+}
