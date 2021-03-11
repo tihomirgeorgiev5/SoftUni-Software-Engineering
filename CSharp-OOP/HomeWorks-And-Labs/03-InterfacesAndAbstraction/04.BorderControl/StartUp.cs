9e32755076c136b3427dcd174b24bbf9da432380
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-03-11 23:52:02 +0300

    Added Exercise in Interfaces

---
+++ CSharp-OOP/HomeWorks-And-Labs/03-InterfacesAndAbstraction/04.BorderControl/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PersonInfo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<IIdentifiable> identifiables = new List<IIdentifiable>();
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+
+                if (line == "End")
+                {
+                    break;
+                }
+
+                string[] parts = line.Split();
+
+                if (parts.Length == 3)
+                {
+                    string name = parts[0];
+                    int age = int.Parse(parts[1]);
+                    string id = parts[2];
+
+                    identifiables.Add(new Citizen(name, age, id));
+                }
+                else
+                {
+                    string model = parts[0];
+                    string id = parts[1];
+
+                    identifiables.Add(new  Robot(id, model));
+                }
+
+
+            }
+
+            string filterId = Console.ReadLine();
+
+            List<IIdentifiable> filtered = identifiables.Where(i => i.Id.EndsWith(filterId)).ToList();
+
+            foreach (var identifiable in filtered )
+            {
+                Console.WriteLine(identifiable.Id);
+            }
+
+
+        }
+    }
+}
