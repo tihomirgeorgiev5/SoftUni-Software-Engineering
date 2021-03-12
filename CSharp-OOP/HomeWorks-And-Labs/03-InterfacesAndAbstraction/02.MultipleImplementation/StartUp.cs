302e8babdf72cad3d08096c41db961dac80d8a45
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-03-13 00:46:52 +0300

    Added Exercise in Interfaces

---
+++ CSharp-OOP/HomeWorks-And-Labs/03-InterfacesAndAbstraction/02.MultipleImplementation/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PersonInfo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            string id = Console.ReadLine();
+            string birthdate = Console.ReadLine();
+            IIdentifiable identifiable = new Citizen(name, age, id, birthdate);
+            IBirthable birthable = new Citizen(name, age, id, birthdate);
+            Console.WriteLine(identifiable.Id);
+            Console.WriteLine(birthable.Birthdate);
+
+
+
+        }
+    }
+}
