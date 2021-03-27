991c6a50339fda0dc2397373f11671f5d2843236
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-03-27 22:59:00 +0300

    Added Exercise for a class Student in Reflection-Lab

---
+++ CSharp-OOP/HomeWorks-And-Labs/06-ReflectionAndAttributes-Lab/Exercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type studentType = typeof(Student);
+
+            Type[] interfaces = studentType.GetInterfaces();
+
+            foreach (var type in interfaces)
+            {
+                Console.WriteLine(type.Name);
+            }
+        }
+    }
+}
---
+++ CSharp-OOP/HomeWorks-And-Labs/06-ReflectionAndAttributes-Lab/Exercise/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise
+{
+    class Student : ICloneable, IComparable
+    {
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Hello()
+        {
+
+        }
+    }
+}
