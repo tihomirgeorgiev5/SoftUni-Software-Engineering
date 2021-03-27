b5ce7e6d49fa7591b80531cea8cbb79b25c74d54
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-03-27 23:43:40 +0300

    Added CreateInstance as Activator

---
+++ CSharp-OOP/HomeWorks-And-Labs/06-ReflectionAndAttributes-Lab/CreateInstance-Exercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace CreateInstance_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type sbType = typeof(StringBuilder);
+            StringBuilder sb = (StringBuilder)Activator.CreateInstance(sbType);
+
+            sb.Append("Hello");
+            Console.WriteLine(sb);
+        }
+    }
+}
