94117bd60f7a536473361d4af4d0351d9e428ac8
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-03-28 11:29:40 +0300

    Added Flags to enum

---
+++ CSharp-OOP/HomeWorks-And-Labs/06-ReflectionAndAttributes-Lab/eNumDays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace eNumDays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Days today = Days.Monday | Days.Tuesday | Days.Wednesday | Days.Thursday;
+
+            Console.WriteLine(today);
+            Console.WriteLine((int)today);
+            Console.WriteLine((Days)15);
+        }
+    }
+    [Flags]
+    enum Days
+    {
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 4,
+        Thursday = 8
+
+    }
+}
