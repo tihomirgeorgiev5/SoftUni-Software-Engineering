b4e2fb5b701799c4c43c9d45cf1f6e85a861dffb
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-03-28 14:44:06 +0300

    Added Reflection-Methods

---
+++ CSharp-OOP/HomeWorks-And-Labs/06-ReflectionAndAttributes-Lab/Reflection-Methods/MathsSoftUni.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reflection_Methods
+{
+    class MathsSoftUni
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        protected int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public virtual int Percentage (int a, int b)
+        {
+            return a / b * 100;
+        }
+    }
+}
---
+++ CSharp-OOP/HomeWorks-And-Labs/06-ReflectionAndAttributes-Lab/Reflection-Methods/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Reflection_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(MathsSoftUni);
+
+            MethodInfo[] methods = type.GetMethods(
+                BindingFlags.NonPublic |
+                BindingFlags.Instance |
+                BindingFlags.Static |
+                BindingFlags.Public);
+
+            MathsSoftUni math = new MathsSoftUni();
+
+            foreach  (MethodInfo method in methods)
+            {
+                ParameterInfo[] methodParamInfos = method.GetParameters();
+
+                var methodParams = methodParamInfos.Select(p => new KeyValuePair<string, string>(p.Name, p.ParameterType.Name)).ToList();
+
+                Console.WriteLine($"{method.Name} -> {string.Join(",",methodParams)}");
+
+                var inputParams = new object[] { 5, 6 };
+
+                if (methodParamInfos.Length > 2)
+                {
+                    inputParams = new object[] { 5, 6, 7 };
+                }
+
+                int result = (int)method.Invoke(math, inputParams);
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
