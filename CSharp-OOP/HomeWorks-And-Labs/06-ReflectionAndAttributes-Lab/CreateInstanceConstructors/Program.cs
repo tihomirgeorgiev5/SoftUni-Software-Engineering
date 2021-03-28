c0693b72e0940465568df1dcf1b01c427df79108
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-03-28 10:57:59 +0300

    Added CreateInstanceConstructors in Exercise

---
+++ CSharp-OOP/HomeWorks-And-Labs/06-ReflectionAndAttributes-Lab/CreateInstanceConstructors/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+namespace CreateInstanceConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            Type studentType = typeof(Student);
+
+            FieldInfo[] fields = studentType.GetFields(
+                BindingFlags.Instance |
+                BindingFlags.Static |
+                BindingFlags.Public |
+                BindingFlags.NonPublic);
+
+            foreach (var field in fields)
+            {
+                Console.WriteLine(field.Name);
+                Console.WriteLine(field.FieldType);
+                Console.WriteLine($"Is public ?{field.IsPublic}");
+
+
+            }
+        }
+    }
+}
