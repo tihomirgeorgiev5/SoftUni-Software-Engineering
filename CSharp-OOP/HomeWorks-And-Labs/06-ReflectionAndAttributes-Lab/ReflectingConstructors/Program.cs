40d1a83ac69f9b798cb8bb74a50eb8addd80b371
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-03-28 13:02:25 +0300

    Added Reflection-Constructors

---
+++ CSharp-OOP/HomeWorks-And-Labs/06-ReflectionAndAttributes-Lab/ReflectingConstructors/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectingConstructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(Student);
+            ConstructorInfo[] constructors = type.GetConstructors();
+
+            foreach (var constructor in constructors)
+            {
+                
+                Console.WriteLine(constructor);
+            }
+        }
+    }
+}
