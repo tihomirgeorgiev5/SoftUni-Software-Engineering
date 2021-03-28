a950b719eb9e16d37d1f44e50aa0da3998cf2a22
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-03-28 19:55:37 +0300

    Added Attribites

---
+++ CSharp-OOP/HomeWorks-And-Labs/06-ReflectionAndAttributes-Lab/Attributes/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Attributes
+{
+    [Serializable]
+    [Obsolete]
+    [Student("Pesho", Name = "Test", CustomProperty = 5)]
+    class Student
+    {
+        [Obsolete]
+        [Student]
+        public void PrintName()
+        {
+
+        }
+
+        [Student]
+        public int Age { get; set; }
+    }
+}
