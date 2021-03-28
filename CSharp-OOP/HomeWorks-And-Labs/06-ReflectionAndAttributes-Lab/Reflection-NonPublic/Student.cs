8c992a0354d6ac02b4ac22e0bb49f2b1d206133a
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-03-28 11:50:18 +0300

    Added GetType.NonPublic

---
+++ CSharp-OOP/HomeWorks-And-Labs/06-ReflectionAndAttributes-Lab/Reflection-NonPublic/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reflection_NonPublic
+{
+    class Student
+    {
+        private static int averageGrade = 2;
+        private static int age = 15;
+        internal string fullName = "Pesho Peshov";
+        public string prqkor = "gotiniq";
+
+        public Student()
+        {
+
+        }
+
+        public Student(string name, int x)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
