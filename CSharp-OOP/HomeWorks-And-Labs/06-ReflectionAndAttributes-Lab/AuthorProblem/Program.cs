643e08171a6c17debc45f53b000ff38caf7a2888
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-03-28 20:13:37 +0300

    Added AuthorProblem

---
+++ CSharp-OOP/HomeWorks-And-Labs/06-ReflectionAndAttributes-Lab/AuthorProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    public class Program
+    {
+        [Author("tissho")]
+        [Author("ti")]
+        [Author("toi")]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
