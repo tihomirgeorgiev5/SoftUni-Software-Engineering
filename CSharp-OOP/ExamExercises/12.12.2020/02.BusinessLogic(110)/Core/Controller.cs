939f49b3df2b9567aabc57ad1475423a928dbb86
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-04-08 02:21:39 +0300

    Added Exercise in ExamPrep

---
+++ CSharp-OOP/ExamExercises/12.12.2020/02.BusinessLogic(110)/Core/Controller.cs
@@ -0,0 +1,162 @@
+﻿using Bakery.Core.Contracts;
+using Bakery.Models.BakedFoods;
+using Bakery.Models.BakedFoods.Contracts;
+using Bakery.Models.Drinks;
+using Bakery.Models.Drinks.Contracts;
+using Bakery.Models.Tables;
+using Bakery.Models.Tables.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Bakery.Utilities.Enums;
+
+namespace Bakery.Core
+{
+    public class Controller : IController
+    {
+        private List<IBakedFood> bakedFoods;
+        private List<IDrink> drinks;
+        private List<ITable> tables;
+        private decimal totalncome = 0;
+
+        public Controller()
+        {
+            bakedFoods = new List<IBakedFood>();
+            drinks = new List<IDrink>();
+            tables = new List<ITable>();
+        }
+
+        public string AddDrink(string type, string name, int portion, string brand)
+        {
+            if (type == "Water")
+            {
+                this.drinks.Add(new Water(name, portion, brand));
+            }
+            if (type == "Tea")
+            {
+                this.drinks.Add(new Tea(name, portion, brand));
+            }
+            return $"Added {name} ({brand}) to the drink menu";
+
+        }
+
+        public string AddFood(string type, string name, decimal price)
+        {
+            if (type == "Bread")
+            {
+                this.bakedFoods.Add(new Bread(name, price));
+            }
+            if (type == "Cake")
+            {
+                this.bakedFoods.Add(new Cake(name, price));
+            }
+            return $"Added {name} ({type}) to the menu";
+        }
+
+        public string AddTable(string type, int tableNumber, int capacity)
+        {
+            if (type == "InsideTable")
+            {
+                this.tables.Add(new InsideTable(tableNumber, capacity));
+            }
+            if (type == "OutsideTable")
+            {
+                this.tables.Add(new OutsideTable(tableNumber, capacity));
+            }
+            return $"Added table number {tableNumber} in the bakery";
+
+        }
+
+        public string GetFreeTablesInfo()
+        {
+            string result = "";
+            List<ITable> freeTables = tables.Where(t => !t.IsReserved).ToList();
+
+            for (int i = 0; i < freeTables.Count; i++)
+            {
+                result += freeTables[i].GetFreeTableInfo() + "\r\n";
+
+            }
+
+            return result.TrimEnd();
+        }
+
+        public string GetTotalIncome()
+        {
+            return $"Total income: {totalncome:f2}lv";
+        }
+
+        public string LeaveTable(int tableNumber)
+        {
+            var table = tables.FirstOrDefault(t => t.TableNumber == tableNumber);
+            var bill = table.GetBill();
+            totalncome += bill;
+            
+            table.Clear();
+            return $"Table: {tableNumber}\r\n" +
+                    $"Bill: {bill:f2}";
+            
+        }
+
+        public string OrderDrink(int tableNumber, string drinkName, string drinkBrand)
+        {
+            var table = tables.FirstOrDefault(t => t.TableNumber == tableNumber);
+            if (table == null)
+            {
+                return $"Could not find table {tableNumber}";
+            }
+            else
+            {
+                var drink = drinks.FirstOrDefault(f => f.Name == drinkName && f.Brand == drinkBrand);
+                if (drink == null)
+                {
+                    return $"There is no {drinkName } {drinkBrand} available";
+
+                }
+                else
+                {
+                    table.OrderDrink(drink);
+                    return $"Table { tableNumber} ordered { drinkName} { drinkBrand}";
+                }
+            }
+        }
+
+        public string OrderFood(int tableNumber, string foodName)
+        {
+            var table = tables.FirstOrDefault(t => t.TableNumber == tableNumber);
+            if (table == null)
+            {
+                return $"Could not find table {tableNumber}";
+            }
+            else
+            {
+                var food = bakedFoods.FirstOrDefault(f => f.Name == foodName);
+                if (food == null)
+                {
+                    return $"No {foodName} in the menu";
+                }
+                else
+                {
+                    table.OrderFood(food);
+                    return $"Table {tableNumber} ordered {foodName}";
+                }
+            }
+        }
+
+        public string ReserveTable(int numberOfPeople)
+        {
+            var table = tables.FirstOrDefault(t => !t.IsReserved && t.Capacity >= numberOfPeople);
+
+            if (table == null)
+            {
+                return $"No available table for {numberOfPeople} people";
+            }
+            else
+            {
+                table.Reserve(numberOfPeople);
+                return $"Table {table.TableNumber} has been reserved for {numberOfPeople} people";
+            }
+        }
+    }
+}
---
+++ CSharp-OOP/ExamExercises/12.12.2020/02.BusinessLogic(110)/Models/BakedFoods/Bread.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bakery.Models.BakedFoods.Contracts
+{
+    public class Bread : BakedFood
+    {
+        public Bread(string name, decimal price) : base(name, 200, price)
+        {
+        }
+    }
+}
