b7ddf77928ea5407b51158aa795bb11a66ede22e
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-04-11 15:11:14 +0300

    Added Exercise in AdvanceRetakeExamExercises

---
+++ CSharp-Advanced/MoreExercises/ExamExercises/C#AdvancedRetakeExam-19August2020/01.FlowerWreathsRetake/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Flower_Wreaths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack<int> lilies = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+
+            Queue<int> roses = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+
+            int flowers = 0;
+            int wreaths = 0;
+
+            while (lilies.Count > 0 && roses.Count > 0)
+            {
+                int lilie = lilies.Pop();
+                int rose = roses.Dequeue();
+
+                int sum = lilie + rose;
+                while (true)
+                {
+                    if (sum == 15)
+                    {
+                        wreaths++;
+                        break;
+
+                    }
+                    else if (sum < 15)
+                    {
+                        flowers += sum;
+                        break;
+
+                    }
+                    else
+                    {
+                        sum -= 2;
+                    }
+
+                }
+
+            }
+            if (flowers >= 15)
+            {
+                wreaths += flowers / 15;
+            }
+            
+
+            if (wreaths >= 5)
+            {
+                Console.WriteLine($"You made it, you are going to the competition with {wreaths} wreaths!");
+            }
+            else
+            {
+                Console.WriteLine($"You didn't make it, you need {5 - wreaths} wreaths more!");
+            }
+
+           
+
+
+
+
+
+
+
+        }
+    }
+}
