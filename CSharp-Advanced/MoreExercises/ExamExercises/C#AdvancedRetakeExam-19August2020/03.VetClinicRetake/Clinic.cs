3a074c4e0292b1fc9c171b1459626f6c0d6ca072
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-04-14 01:21:28 +0300

    Added Exercise in ExamRetakeAdvanced

---
+++ CSharp-Advanced/MoreExercises/ExamExercises/C#AdvancedRetakeExam-19August2020/03.VetClinicRetake/Clinic.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> data;
+
+        public Clinic(int capacity)
+        {
+            Capacity = capacity;
+            data = new List<Pet>();
+           
+        }
+
+        public int Capacity { get; set; }
+
+        public int Count { get { return data.Count; } }
+
+        public void Add(Pet pet)
+        {
+            if (data.Count < Capacity)
+            {
+                data.Add(pet);
+            }
+            
+        }
+
+        public bool Remove(string name)
+        {
+            Pet pet = data.FirstOrDefault(d => d.Name == name);
+            if (pet == null)
+            {
+                return false;
+            }
+            data.Remove(pet);
+            return true;
+        }
+
+        public Pet GetPet(string name, string owner)
+        {
+            Pet pet = data.FirstOrDefault(p => p.Name == name && p.Owner == owner);
+            return pet;
+
+        }
+
+        public Pet GetOldestPet()
+        {
+            return data.OrderByDescending(p => p.Age).FirstOrDefault();
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("The clinic has the following patients:");
+
+            foreach (var Pet in data)
+            {
+                sb.AppendLine($"Pet {Pet.Name} with owner: {Pet.Owner}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
