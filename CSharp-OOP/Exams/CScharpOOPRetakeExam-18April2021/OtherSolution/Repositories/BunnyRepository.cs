646f2d62f633f335f73687b2872d4a499acb1efe
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-05-01 23:58:34 +0300

    Added Folder of other solution of Exam

---
+++ CSharp-OOP/Exams/CScharpOOPRetakeExam-18April2021/OtherSolution/Repositories/BunnyRepository.cs
@@ -0,0 +1,36 @@
+﻿using Easter.Models.Bunnies.Contracts;
+using Easter.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Easter.Repositories
+{
+    public class BunnyRepository : IRepository<IBunny>
+    {
+        private List<IBunny> models;
+
+        public BunnyRepository()
+        {
+            this.models = new List<IBunny>();
+        }
+        public IReadOnlyCollection<IBunny> Models => this.models.AsReadOnly();
+
+        public void Add(IBunny model)
+        {
+            models.Add(model);
+        }
+
+        public IBunny FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.Name == name);
+        }
+
+        public bool Remove(IBunny model)
+        {
+            return models.Remove(model);
+        }
+    }
+}
+© 2021 GitHub, Inc.
---
+++ CSharp-OOP/Exams/CScharpOOPRetakeExam-18April2021/OtherSolution/Repositories/EggRepository.cs
@@ -0,0 +1,35 @@
+﻿using Easter.Models.Eggs.Contracts;
+using Easter.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Easter.Repositories
+{
+    public class EggRepository : IRepository<IEgg>
+    {
+        private List<IEgg> models;
+
+        public EggRepository()
+        {
+            this.models = new List<IEgg>();
+        }
+        public IReadOnlyCollection<IEgg> Models => this.models;
+
+        public void Add(IEgg model)
+        {
+            models.Add(model);
+        }
+
+        public IEgg FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.Name == name);
+        }
+
+        public bool Remove(IEgg model)
+        {
+            return models.Remove(model);
+        }
+    }
+}
