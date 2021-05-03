59fcd8c9c4cd75db881e4ab8c9a5bb86b4fcbfe8
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-05-03 20:53:01 +0300

    Added a folder in a other solution of examretake

---
+++ CSharp-OOP/Exams/CScharpOOPRetakeExam-18April2021/OtherSolution/Models/Bunnies/HappyBunny.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easter.Models.Bunnies
+{
+    public class HappyBunny : Bunny
+    {
+        public HappyBunny(string name)
+            : base(name, 100)
+        {
+        }
+
+        public override void Work()
+        {
+            if (Energy - 10 < 0)
+            {
+                Energy = 0;
+            }
+            else
+            {
+                Energy -= 10;
+            }
+        }
+    }
+}
---
+++ CSharp-OOP/Exams/CScharpOOPRetakeExam-18April2021/OtherSolution/Models/Bunnies/SleepyBunny.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easter.Models.Bunnies
+{
+    public class SleepyBunny : Bunny
+    {
+        public SleepyBunny(string name)
+            : base(name, 50)
+        {
+        }
+
+        public override void Work()
+        {
+            if (Energy - 15 < 0)
+            {
+                Energy = 0;
+            }
+            else
+            {
+                Energy -= 15;
+            }
+        }
+    }
+}
