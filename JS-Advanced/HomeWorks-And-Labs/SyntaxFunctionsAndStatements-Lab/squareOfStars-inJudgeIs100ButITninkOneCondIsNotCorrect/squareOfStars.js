765ae951d6d76565f10f7ceb02ef17641b67f7e1
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-05-23 18:57:17 +0300

    Added new exc in syntax-lab in js

---
+++ JS-Advanced/HomeWorks-And-Labs/SyntaxFunctionsAndStatements-Lab/squareOfStars-inJudgeIs100ButITninkOneCondIsNotCorrect/squareOfStars.js
@@ -0,0 +1,12 @@
+function size(stars) {
+    if(stars === '') {
+        stars === 5;
+        
+    } else {
+        for (i = 0; i < stars; i++)  console.log('* '.repeat(stars));
+
+    }
+    
+    
+  }
+size(5)
