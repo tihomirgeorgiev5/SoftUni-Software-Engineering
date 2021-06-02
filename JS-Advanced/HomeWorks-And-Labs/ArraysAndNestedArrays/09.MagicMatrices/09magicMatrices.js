f648b29af13f64cf1483b50dca74622d386e2107
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-06-03 00:10:39 +0300

    Added last regular Problem in Arrays-exc in js

---
+++ JS-Advanced/HomeWorks-And-Labs/ArraysAndNestedArrays/09.MagicMatrices/09magicMatrices.js
@@ -0,0 +1,42 @@
+function solve(matrix) {
+    let magicSum = Number.MIN_SAFE_INTEGER;
+
+    let isMagicMatrix = true;;
+
+
+    for (let row = 0; row < matrix.length; row++) {
+        let sum = matrix[row].reduce((a, x) => a + x);
+
+        if (magicSum === Number.MIN_SAFE_INTEGER) {
+            magicSum = sum;
+
+        }
+
+        if (sum !== magicSum) {
+            isMagicMatrix = false;
+
+        }
+    }
+
+    for (let col = 0; col < matrix[0].length; col++) {
+        let sum = 0;
+        for (let row = 0; row < matrix.length; row++) {
+            let el = matrix[row][col];
+            sum += el;
+            
+        }
+
+        if (sum !== magicSum) {
+            isMagicMatrix = false;
+        }
+        
+
+    }
+    
+
+    return (isMagicMatrix);
+
+}
+console.log(solve([[4, 5, 6],
+[6, 5, 4],
+[5, 5, 5]]))
