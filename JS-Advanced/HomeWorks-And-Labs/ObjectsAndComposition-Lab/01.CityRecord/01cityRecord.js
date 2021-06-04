ec497aa3feeb50255f5770557014387becb0d01d
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-06-04 23:53:54 +0300

    Added new exc in Objects-lab in js

---
+++ JS-Advanced/HomeWorks-And-Labs/ObjectsAndComposition-Lab/01.CityRecord/01cityRecord.js
@@ -0,0 +1,13 @@
+function solve(name, population, treasury){ 
+    let town = {
+        name: name,
+        population: population,
+        treasury: treasury,
+    }
+
+    return town;
+
+}
+console.log(solve('Tortuga',
+7000,
+15000));
