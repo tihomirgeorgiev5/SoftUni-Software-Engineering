aa22d4c3fc852b41af538467d2a61ab7ec1fd995
tihomirgeorgiev5 <georgiev.tm@abv.bg>
2021-07-05 22:24:14 +0200

    Added second lab-exercise in async-programming in js-apps

---
+++ JS-Application/HomeWorks-And-Labs/02.AsyncProgramming-Lab/02.Github-Repos/scripts/app.js
@@ -0,0 +1,34 @@
+function loadRepos() {
+	const username = document.getElementById('username').value;
+
+	const url = `https://api.github.com/users/${username}/repos`;
+
+	 fetch(url)
+	 .then(response => response.json())
+	 .then(data => {
+		 const ulElement  = document.getElementById('repos');
+		 ulElement.innerHTML = '';
+		 data.forEach(r => {
+			 const liElement = document.createElement('li');
+			 liElement.textContent = r.full_name;
+			 ulElement.appendChild(liElement); 
+		 });
+	 });
+
+	/*console.log(requestPromise);
+
+	requestPromise.then(handleResponse);
+
+
+	function handleResponse(response) {
+		console.log(response);
+		const dataPromise = response.json();
+		dataPromise.then(handleData);
+	}
+
+	function handleData(data) {
+		console.log(data);
+	} */
+
+
+}
