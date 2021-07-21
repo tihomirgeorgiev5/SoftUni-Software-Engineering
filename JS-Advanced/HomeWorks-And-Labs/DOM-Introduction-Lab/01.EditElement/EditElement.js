97e11de9ad6dce0fdc0029ab325474dd791fa8b5
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-07-21 20:20:47 +0200

    Added 01.EditElement

---
+++ JS-Advanced/HomeWorks-And-Labs/DOM-Introduction-Lab/01.EditElement/EditElement.js
@@ -0,0 +1,6 @@
+function edit(ref, match, replacer) {
+    const matcher = new RegExp(match, 'g');
+    const result = ref.textContent.replace(matcher, replacer);
+    ref.textContent = result;
+
+}
