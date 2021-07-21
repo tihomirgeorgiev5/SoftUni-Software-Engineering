38f6c42be15eb0464f03274f6d0076e7771ab825
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-07-21 21:07:59 +0200

    Added 04.ShowMore

---
+++ JS-Advanced/HomeWorks-And-Labs/DOM-Introduction-Lab/04.ShowMore/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    const element = document.getElementById('text');
+    element.style.display = 'inline';
+    const hiddenElement = document.getElementById('more');
+    hiddenElement.style.display = 'none';
+}
