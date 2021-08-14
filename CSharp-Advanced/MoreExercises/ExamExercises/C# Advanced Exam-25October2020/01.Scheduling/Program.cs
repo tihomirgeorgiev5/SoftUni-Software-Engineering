75ec3c7844060bdf2fff3514cbbda21b76a54f2d
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-08-14 19:30:14 +0200

    Added new ExamRetakeProblem 01.Scheduling

---
+++ CSharp-Advanced/MoreExercises/ExamExercises/C# Advanced Exam-25October2020/01.Scheduling/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> tasks = new Stack<int>(Console.ReadLine()
+                .Split(", ").Select(int.Parse).ToArray());
+
+            Queue<int> threads = new Queue<int>(Console.ReadLine()
+                            .Split(" ").Select(int.Parse).ToArray());
+
+            int taskToKill = int.Parse(Console.ReadLine());
+
+            while(true)
+            {
+                if(tasks.Peek() == taskToKill)
+                {
+                    break;
+                }
+                if(threads.Peek() >= tasks.Peek())
+                {
+                    tasks.Pop();
+                    threads.Dequeue();
+                }
+                else if(threads.Peek() < tasks.Peek())
+                {
+                    threads.Dequeue();
+
+                }
+            }
+
+            Console.WriteLine($"Thread with value {threads.Peek()} killed task {taskToKill}");
+            Console.WriteLine(string.Join(" ", threads));
+
+            
+        }
+    }
+}
