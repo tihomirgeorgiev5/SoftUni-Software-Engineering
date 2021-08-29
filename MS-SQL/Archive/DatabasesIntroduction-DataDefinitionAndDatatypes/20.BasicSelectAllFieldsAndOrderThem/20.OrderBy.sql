005a80956de382274d65c51efc03b63f98e59bc6
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-08-29 19:40:10 +0200

    Added Problem 20.Order

---
+++ MS-SQL/Archive/DatabasesIntroduction-DataDefinitionAndDatatypes/20.BasicSelectAllFieldsAndOrderThem/20.OrderBy.sql
@@ -0,0 +1,20 @@
+--20.Basic Select All Fields and Order Them 
+SELECT * FROM Towns
+--Towns - alphabetically by name 
+ORDER BY [Name]
+
+SELECT * FROM Departments
+--Departments - alphabetically by name
+ORDER BY [Name]
+
+SELECT * FROM Employees
+--Employees - descending by salary
+ORDER BY Salary DESC
+
+
+
+
+
+
+
+
