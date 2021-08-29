6f0ade20b430b8efddff76ff90aa7c5c5f9f7be4
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-08-29 19:55:36 +0200

    Added 22.SetSalary

---
+++ MS-SQL/Archive/DatabasesIntroduction-DataDefinitionAndDatatypes/22.IncreaseEmployeesSalary/22.SetSalary.sql
@@ -0,0 +1,16 @@
+--22.Increase Employees Salary 
+
+--Use SoftUni database and increase the salary of all employees by 10%. 
+--Then show only Salary column for all in the Employees table
+
+UPDATE Employees
+SET Salary *= 1.1
+
+SELECT Salary FROM Employees
+
+
+
+
+
+
+
