71a89f32feeb87225b9c1ae89eaadbe4b7972aae
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-10-12 18:33:00 +0200

    Added Joins_Exercise

---
+++ MS-SQL/DatabasesBasics-Exercises/SubqueriesАndJoins/03. Sales Employees.sql
@@ -0,0 +1,12 @@
+USE [Softuni]
+
+SELECT e.[EmployeeID],
+       e.[FirstName],
+	   e.[LastName],
+	   d.[Name] AS [DepartmentName]
+FROM [Employees] AS e
+LEFT JOIN [Departments] AS d
+ON e.[DepartmentID] = d.[DepartmentID]
+WHERE d.[Name] = 'Sales'
+ORDER BY e.[EmployeeID]
+         
