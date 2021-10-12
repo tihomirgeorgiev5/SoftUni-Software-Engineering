638c3c7384eac12b331ed62dc918da032b54db97
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-10-12 19:16:47 +0200

    Added Joins_Exercise

---
+++ MS-SQL/DatabasesBasics-Exercises/SubqueriesАndJoins/06. Employees Hired After.sql
@@ -0,0 +1,14 @@
+USE [Softuni]
+
+SELECT e.[FirstName],
+       e.[LastName],
+	   e.[HireDate],
+	   d.[Name] AS [DeptName]
+FROM [Employees] AS e
+LEFT JOIN [Departments] AS d
+ON e.[DepartmentID] = d.[DepartmentID]
+WHERE e.[HireDate] > '1999-01-01'
+AND d.[Name] IN ('Sales','Finance')
+ORDER BY e.[HireDate]
+
+         
