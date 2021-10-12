36e9a2860c41415e25adcf3f937f2d0af5495922
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-10-12 21:25:30 +0200

    Added Joins_Exercise

---
+++ MS-SQL/DatabasesBasics-Exercises/SubqueriesАndJoins/08. Employee 24.sql
@@ -0,0 +1,17 @@
+USE [Softuni]
+
+SELECT  e.[EmployeeID],
+              e.[FirstName],
+			  CASE 
+			  WHEN YEAR( p.[StartDate]) >= 2005 THEN NULL
+			  ELSE p.[Name] 
+			  END AS [ProjectName]
+FROM [Employees] AS e
+ JOIN [EmployeesProjects] AS ep
+ON e.[EmployeeID] = ep.[EmployeeID]
+ JOIN [Projects] AS p
+ON p.[ProjectID] = ep.[ProjectID]
+WHERE e.[EmployeeID] = 24 
+
+
+         
