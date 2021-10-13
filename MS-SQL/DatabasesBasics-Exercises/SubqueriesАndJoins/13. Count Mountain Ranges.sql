00e4a7d2e020ab67d4df1c06b5920443ee9b5d3d
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-10-13 19:58:30 +0200

    Added Joins_Exercise

---
+++ MS-SQL/DatabasesBasics-Exercises/SubqueriesАndJoins/13. Count Mountain Ranges.sql
@@ -0,0 +1,21 @@
+USE [Geography]
+
+SELECT c.[CountryCode],
+       COUNT (mc.[MountainId]) AS [MountainRanges]
+      
+       
+ FROM [Countries] AS c
+LEFT JOIN [MountainsCountries] AS mc
+ON c.[CountryCode] = mc.[CountryCode]
+WHERE c.[CountryCode] IN('BG','RU','US')
+GROUP BY c.[CountryCode]
+
+
+
+
+    
+     
+ 
+
+
+         
