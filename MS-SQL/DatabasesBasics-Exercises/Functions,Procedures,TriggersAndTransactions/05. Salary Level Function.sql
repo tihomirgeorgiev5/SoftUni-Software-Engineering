e40a5135fe69fb074f7258565ca55cced174878d
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-10-21 02:14:09 +0200

    Added new Exercise in Functions

---
+++ MS-SQL/DatabasesBasics-Exercises/Functions,Procedures,TriggersAndTransactions/05. Salary Level Function.sql
@@ -0,0 +1,27 @@
+-- 05. Salary Level Function
+CREATE FUNCTION ufn_GetSalaryLevel(@salary DECIMAL(18,4))
+RETURNS VARCHAR (7)
+AS
+BEGIN
+DECLARE @salaryLevel VARCHAR(7)
+IF @salary < 30000
+BEGIN
+SET @salaryLevel = 'Low'
+END
+ELSE IF @salary BETWEEN 30000 AND 50000
+BEGIN
+SET @salaryLevel = 'Average'
+END
+ELSE 
+BEGIN
+SET @salaryLevel = 'High'
+END
+RETURN @salaryLevel
+
+END
+
+--SELECT e.[Salary],
+       --[dbo].[ufn_GetSalaryLevel]([Salary]) AS [SalaryLevel]
+        
+--FROM [Employees] AS e
+
