e0bae7a7ad3542460e8347e80a016a052ff16042
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-11-12 21:28:37 +0100

    Added first exercise of LINQ

---
+++ Entity-Framework-Core/Exercises/LINQ/01.MusicHubDatabase/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace MusicHub
+{
+    using System;
+
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            MusicHubDbContext context = 
+                new MusicHubDbContext();
+
+            DbInitializer.ResetDatabase(context);
+
+            //Test your solutions here
+            Console.WriteLine("Db created successfully!");
+        }
+
+        public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
