c03f2e41a99a1e2caef0a876874abe6af58ace4e
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-11-20 23:13:20 +0100

    Added second Exercise from JSON

---
+++ Entity-Framework-Core/Exercises/JavaScriptObjectNotation-JSON/02.ImportProducts/ProductShopProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ProductShop.DataTransferObject;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            this.CreateMap<UserInputModel, User>();
+
+            this.CreateMap<ProductInputModel, Product>();
+        }
+    }
+}
