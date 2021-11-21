5587f16e62fdea06adaae511a6164472a085d03a
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-11-21 10:27:48 +0100

    Added first exercise from Export in Json

---
+++ Entity-Framework-Core/Exercises/JavaScriptObjectNotation-JSON/05.ExportProductsInRange/ProductShopProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ProductShop.DataTransferObject;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            this.CreateMap<UserInputModel, User>();
+
+            this.CreateMap<ProductInputModel, Product>();
+
+            this.CreateMap<CategorieInputModel, Category>();
+
+            this.CreateMap<CatProductInputModel, CategoryProduct>();
+        }
+    }
+}
