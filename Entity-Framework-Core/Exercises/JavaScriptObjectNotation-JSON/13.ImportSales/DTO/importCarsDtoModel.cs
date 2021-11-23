ea66dc654ab3a22338e62638404ac65768df909c
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-11-23 01:27:25 +0100

    Added Problem.13 of Json

---
+++ Entity-Framework-Core/Exercises/JavaScriptObjectNotation-JSON/13.ImportSales/DTO/importCarsDtoModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarDealer.DTO
+{
+    public class importCarsDtoModel
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public long TravelledDistance { get; set; }
+
+        public IEnumerable<int> PartsId { get; set; }
+
+
+
+    }
+    
+}
---
+++ Entity-Framework-Core/Exercises/JavaScriptObjectNotation-JSON/13.ImportSales/StartUp.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTO;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var context = new CarDealerContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            var suppliersJson = File.ReadAllText("../../../Datasets/suppliers.json");
+            var partsJson = File.ReadAllText("../../../Datasets/parts.json");
+            var carsJson = File.ReadAllText("../../../Datasets/cars.json");
+            var customersJson = File.ReadAllText("../../../Datasets/customers.json");
+            var salesJson = File.ReadAllText("../../../Datasets/sales.json");
+
+            Console.WriteLine(ImportSuppliers(context,suppliersJson));
+            Console.WriteLine(ImportParts(context, partsJson));
+            Console.WriteLine(ImportCars(context,carsJson));
+            Console.WriteLine(ImportCustomers(context,customersJson));
+            Console.WriteLine(ImportSales(context,salesJson));
+        }
+
+        public static string ImportSales(CarDealerContext context, string inputJson)
+        {
+
+            var sales = JsonConvert
+                .DeserializeObject<Sale[]>(inputJson);
+
+            context.Sales.AddRange(sales);
+            context.SaveChanges();
+
+            return $"Successfully imported {sales.Count()}.";
+        }
+        public static string ImportCustomers(CarDealerContext context, string inputJson)
+        {
+            var customers = JsonConvert
+                .DeserializeObject<Customer[]>(inputJson);
+
+            context.Customers.AddRange(customers);
+            context.SaveChanges();
+
+            return $"Successfully imported {customers.Count()}.";
+
+        }
+
+        public static string ImportCars(CarDealerContext context, string inputJson)
+        {
+            var carsDto = JsonConvert.DeserializeObject<IEnumerable<importCarsDtoModel>>(inputJson);
+
+            var listOfCars = new List<Car>();
+
+            foreach (var car in carsDto)
+            {
+                var currentCar = new Car
+                {
+                    Make = car.Make,
+                    Model = car.Model,
+                    TravelledDistance = car.TravelledDistance
+                };
+
+                foreach (var partId in car?.PartsId.Distinct())
+                {
+                    currentCar.PartCars.Add(new PartCar
+                    {
+                        PartId = partId
+                    });
+
+                }
+
+                listOfCars.Add(currentCar);
+
+            }
+
+            context.Cars.AddRange(listOfCars);
+            context.SaveChanges();
+
+            return $"Successfully imported {listOfCars.Count}.";
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            var supplliedIds = context.Suppliers
+                .Select(x => x.Id)
+                .ToArray();
+
+            var parts = JsonConvert.DeserializeObject<IEnumerable<Part>>(inputJson)
+                .Where(p => supplliedIds.Contains(p.SupplierId))
+                .ToList();
+
+            context.Parts.AddRange(parts);
+            context.SaveChanges();
+
+            return $"Successfully imported {parts.Count()}.";
+
+        }
+
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var suppliersDto = JsonConvert.DeserializeObject<IEnumerable<importSupplierDtoModel>>(inputJson);
+
+            var suppliers = suppliersDto.Select(x => new Supplier
+            {
+                Name = x.Name,
+                IsImporter = x.IsImporter
+            })
+                .ToArray();
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count()}.";
+
+
+        }
+    }
+}
