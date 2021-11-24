edef0d7038ec209065076bf3ffade33886c22c08
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-11-24 01:48:12 +0100

    Added Problem.14 of Json

---
+++ Entity-Framework-Core/Exercises/JavaScriptObjectNotation-JSON/14.ExportOrderedCustomers/DTO/importCustomersDtoModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarDealer.DTO
+{
+    public class importCustomersDtoModel
+    {
+        public string Name { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public bool IsYoungDriver { get; set; }
+
+    }
+
+   
+}
