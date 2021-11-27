00cef4d1668c47512b30ed39c58e5e18e0843062
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-11-27 17:11:32 +0100

    Added Promlem.09 of XML

---
+++ Entity-Framework-Core/Exercises/ExtensibleMarkupLanguage-XML/09.ImportSuppliers/StartUp.cs
@@ -0,0 +1,65 @@
+﻿using CarDealer.Data;
+using CarDealer.DTO.ImportDto;
+using CarDealer.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            CarDealerContext dbContext = new CarDealerContext();
+
+            //ResetDb(dbContext);
+
+            string inputXml = File.ReadAllText("../../../Datasets/suppliers.xml");
+            string result = ImportSuppliers(dbContext, inputXml);
+
+            Console.WriteLine(result);
+
+        }
+
+        private static void ResetDb(CarDealerContext dbContext)
+        {
+            dbContext.Database.EnsureDeleted();
+            dbContext.Database.EnsureCreated();
+
+            Console.WriteLine("Db reset successfully");
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute("Suppliers");
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportSupplierDto[]), xmlRoot);
+
+            using StringReader stringReader = new StringReader(inputXml);
+
+            ImportSupplierDto[] dtos = (ImportSupplierDto[])xmlSerializer.Deserialize(stringReader);
+
+            ICollection<Supplier> suppliers = new HashSet<Supplier>();
+
+            foreach (var supplierDto in dtos)
+            {
+                Supplier s = new Supplier()
+                {
+                    Name = supplierDto.Name,
+                    IsImporter = bool.Parse(supplierDto.IsImporter)
+                };
+
+                suppliers.Add(s);
+
+            }
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}";
+
+        }
+    }
+}
