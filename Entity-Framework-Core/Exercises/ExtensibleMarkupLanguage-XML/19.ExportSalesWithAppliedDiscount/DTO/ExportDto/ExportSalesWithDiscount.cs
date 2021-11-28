537fd9d770d2bf4b7ed5e06b124b1ef5a9023cc9
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-11-28 14:40:03 +0100

    Added Problem.19 of Xml

---
+++ Entity-Framework-Core/Exercises/ExtensibleMarkupLanguage-XML/19.ExportSalesWithAppliedDiscount/DTO/ExportDto/ExportSalesWithDiscount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTO.ExportDto
+{
+    [XmlType("sale")]
+    public class ExportSalesWithDiscount
+    {
+        [XmlElement("car")]
+        public ExportSalesCarDto Car { get; set; }
+
+        [XmlElement("discount")]
+        public string Discount { get; set; }
+
+        [XmlElement("customer-name")]
+        public string CustomerName { get; set; }
+
+        [XmlElement("price")]
+        public string Price { get; set; }
+
+        [XmlElement("price-with-discount")]
+        public string PriceWithDiscount { get; set; }
+    }
+}
