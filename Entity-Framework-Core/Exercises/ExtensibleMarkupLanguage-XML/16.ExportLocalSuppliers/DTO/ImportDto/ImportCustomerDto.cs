b4bdbfcfc470dfdbe73adc50fe3d99ba90b180c3
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-11-29 00:40:20 +0100

    Added Problem.16 of Xml

---
+++ Entity-Framework-Core/Exercises/ExtensibleMarkupLanguage-XML/16.ExportLocalSuppliers/DTO/ImportDto/ImportCustomerDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTO.ImportDto
+{
+    [XmlType("Customer")]
+    public class ImportCustomerDto
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+        [XmlElement("birthDate")]
+        public string BirthDate { get; set; }
+        [XmlElement("isYoungDriver")]
+        public string IsYoungDriver { get; set; }
+    }
+
+   // <Customer>
+   //    <name>Emmitt Benally</name>
+   //    <birthDate>1993-11-20T00:00:00</birthDate>
+   //    <isYoungDriver>true</isYoungDriver>
+   //</Customer>
+}
