22ca067d056cedb6151c1b1bfe63a39408176403
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-11-28 14:36:53 +0100

    Added Problem.19 of Xml

---
+++ Entity-Framework-Core/Exercises/ExtensibleMarkupLanguage-XML/19.ExportSalesWithAppliedDiscount/DTO/ExportDto/ExportSalesCarDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTO.ExportDto
+{
+    [XmlType("car")]
+    public class ExportSalesCarDto
+    {
+        [XmlAttribute("make")]
+        public string Make { get; set; }
+
+        [XmlAttribute("model")]
+        public string Model { get; set; }
+
+        [XmlAttribute("travelled-distance")]
+        public string TraveledDistance { get; set; }
+    }
+}
