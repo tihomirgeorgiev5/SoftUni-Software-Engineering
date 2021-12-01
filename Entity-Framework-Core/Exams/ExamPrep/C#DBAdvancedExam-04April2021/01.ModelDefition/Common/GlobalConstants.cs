f8d2100c3f496d7883c8011bc8aa700a3d3965ad
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-12-02 00:29:19 +0100

    Added ExamPrep in Entity_FirstExercise

---
+++ Entity-Framework-Core/Exams/ExamPrep/C#DBAdvancedExam-04April2021/01.ModelDefition/Common/GlobalConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeisterMask.Common
+{
+    public static class GlobalConstants
+    {
+        public const int EMPLOYEE_USERNAME_MAX_LENGTH = 40;
+
+        public const int EMPLOYEE_PHONE_MAX_LENGTH = 12;
+
+        public const int PROJECT_NAME_MAX_LENGTH = 40;
+
+        public const int TASK_NAME_MAX_LENGTH = 40;
+    }
+}
