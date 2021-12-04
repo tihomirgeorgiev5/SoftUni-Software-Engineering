0517d86c65f9b70ab520a53fab5897ed8eb01c3a
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-12-04 10:20:33 +0100

    Added 03.ExportData from ExamPrep

---
+++ Entity-Framework-Core/Exams/ExamPrep/C#DBAdvancedExam-04April2021/03.DataExport/DataProcessor/ImportDto/ImportProjectTaskDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+using TeisterMask.Common;
+
+namespace TeisterMask.DataProcessor.ImportDto
+{
+    [XmlType("Task")]
+    public class ImportProjectTaskDto
+    {
+        [XmlElement("Name")]
+        [Required]
+        [MaxLength(GlobalConstants.TASK_NAME_MAX_LENGTH)]
+        [MinLength(GlobalConstants.TASK_NAME_MIN_LENGTH)]
+
+        public string Name { get; set; }
+
+        [XmlElement("OpenDate")]
+        [Required]
+        public string TaskOpenDate { get; set; }
+
+        [XmlElement("DueDate")]
+        [Required]
+        public string TaskDueDate { get; set; }
+
+        [XmlElement("ExecutionType")]
+        [Range(GlobalConstants.TASK_EXEC_TYPE_MIN_VALUE,GlobalConstants.TASK_EXEC_TYPE_MAX_VALUE)]
+        public int ExecutionType { get; set; }
+
+        [XmlElement("LabelType")]
+        [Range(GlobalConstants.TASK_LABEL_TYPE_MIN_VALUE,GlobalConstants.TASK_LABEL_TYPE_MAX_VALUE)]
+
+        public int LabelType { get; set; }
+    }
+}
