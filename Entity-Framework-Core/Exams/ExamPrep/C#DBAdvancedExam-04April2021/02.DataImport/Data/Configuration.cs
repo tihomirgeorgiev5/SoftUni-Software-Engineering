64fcbe81a37ecbc35b94a8f137ea30ead77201e9
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-12-04 10:13:42 +0100

    Added 02.DataImport from ExamPrep

---
+++ Entity-Framework-Core/Exams/ExamPrep/C#DBAdvancedExam-04April2021/02.DataImport/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace TeisterMask.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString = @"Server=DESKTOP-SB9MJ7T\SQLEXPRESS;Database=TeisterMask;Trusted_Connection=True";
+    }
+}
---
+++ Entity-Framework-Core/Exams/ExamPrep/C#DBAdvancedExam-04April2021/02.DataImport/DataProcessor/ImportDto/ImportEmployeeDto.cs
@@ -0,0 +1,27 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using TeisterMask.Common;
+
+namespace TeisterMask.DataProcessor.ImportDto
+{
+    public class ImportEmployeeDto
+    {
+        [Required]
+        [MinLength(GlobalConstants.EMPLOYEE_USERNAME_MIN_LENGTH)]
+        [MaxLength(GlobalConstants.EMPLOYEE_USERNAME_MAX_LENGTH)]
+        [RegularExpression(GlobalConstants.EMPLOYEE_USERNAME_REGEX)]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        
+        public string Email { get; set; }
+
+        [RegularExpression(GlobalConstants.EMPLOYEE_PHONE_REGEX)]
+        public string Phone { get; set; }
+
+        public int[] Tasks { get; set; }
+
+
+    }
+}
