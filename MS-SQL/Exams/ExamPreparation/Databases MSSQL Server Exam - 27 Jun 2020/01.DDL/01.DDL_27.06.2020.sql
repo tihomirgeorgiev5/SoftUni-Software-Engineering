7f6f516e9f66b462c1c857bb7b188c98ced28784
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-12-07 20:32:48 +0100

    Added first Problem of ExamPrep Ms SQL

---
+++ MS-SQL/Exams/ExamPreparation/Databases MSSQL Server Exam - 27 Jun 2020/01.DDL/01.DDL_27.06.2020.sql
@@ -0,0 +1,74 @@
+CREATE DATABASE [WMS]
+
+USE [WMS]
+
+CREATE TABLE [Clients](
+    [ClientId] INT PRIMARY KEY IDENTITY NOT NULL,
+    [FirstName] VARCHAR(50) NOT NULL,
+	[LastName] VARCHAR(50) NOT NULL,
+	[Phone] CHAR(12) NOT NULL,
+	CHECK(LEN([Phone]) = 12)
+)
+
+CREATE TABLE [Mechanics](
+    [MechanicId] INT PRIMARY KEY IDENTITY NOT NULL,
+	[FirstName] VARCHAR(50) NOT NULL,
+	[LastName] VARCHAR(50) NOT NULL,
+	[Address] VARCHAR(255) NOT NULL
+)
+
+CREATE TABLE [Models](
+    [ModelId] INT PRIMARY KEY IDENTITY NOT NULL,
+	[Name] VARCHAR(50) UNIQUE NOT NULL)
+
+CREATE TABLE [Jobs](
+    [JobId] INT PRIMARY KEY IDENTITY NOT NULL,
+	[ModelId] INT FOREIGN KEY REFERENCES [Models]([ModelId]) NOT NULL,
+	[Status] VARCHAR(11) NOT NULL DEFAULT('Pending'),
+	[ClientId] INT FOREIGN KEY REFERENCES [Clients]([ClientId]) NOT NULL,
+	[MechanicId] INT FOREIGN KEY REFERENCES [Mechanics]([MechanicId]),
+	[IssueDate] DATETIME2 NOT NULL,
+	[FinishDate] DATETIME2,
+	CHECK ([Status] IN ('Pending', 'In Progress', 'Finished'))
+	)
+	
+CREATE TABLE [Orders](
+[OrderId] INT PRIMARY KEY IDENTITY NOT NULL,
+[JobId] INT FOREIGN KEY REFERENCES [Jobs]([JobId]) NOT NULL,
+[IssueDate] DATETIME2,
+[Delivered] BIT NOT NULL DEFAULT(0)
+) 
+
+CREATE TABLE [Vendors](
+[VendorId] INT PRIMARY KEY IDENTITY NOT NULL,
+[Name] VARCHAR(50) UNIQUE NOT NULL)
+
+CREATE  TABLE [Parts](
+[PartId] INT PRIMARY KEY IDENTITY NOT NULL,
+[SerialNumber] VARCHAR(50) UNIQUE NOT NULL,
+[Description] VARCHAR(255),
+[Price] DECIMAL(6,2) NOT NULL,
+[VendorId]INT FOREIGN KEY REFERENCES [Vendors]([VendorId]) NOT NULL,
+[StockQty] INT NOT NULL DEFAULT(0),
+CHECK ([Price] > 0),
+CHECK ([StockQty] >= 0)
+)
+
+CREATE TABLE [OrderParts](
+[OrderId] INT FOREIGN KEY REFERENCES [Orders] ([OrderId]) NOT NULL,
+[PartId] INT FOREIGN KEY REFERENCES [Parts] ([PartId]) NOT NULL,
+[Quantity] INT NOT NULL DEFAULT(1),
+PRIMARY KEY ([OrderId], [PartId]),
+CHECK ([Quantity] > 0))
+
+CREATE TABLE [PartsNeeded](
+[JobId] INT FOREIGN KEY REFERENCES [Jobs] ([JobId]) NOT NULL,
+[PartId] INT FOREIGN KEY REFERENCES [Parts] ([PartId]) NOT NULL,
+[Quantity] INT NOT NULL DEFAULT(1),
+PRIMARY KEY ([JobId], [PartId]),
+CHECK ([Quantity] > 0))
+
+GO
+´
+
+
