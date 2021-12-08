ad614bce1c79e99c68ea2e75a8a313334ef2878a
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-12-08 19:47:31 +0100

    Added Problem.08-better Solution

---
+++ MS-SQL/Exams/ExamPreparation/Databases MSSQL Server Exam - 27 Jun 2020/08.AvailableMechanics_easy_solve.sql
@@ -0,0 +1,7 @@
+
+
+SELECT CONCAT ([FirstName],' ', [LastName]) AS [Available] 
+FROM [Mechanics]
+WHERE [MechanicId] NOT IN (SELECT [MechanicId] FROM [Jobs]
+WHERE [Status] = 'In Progress'
+GROUP BY [MechanicId]) 
