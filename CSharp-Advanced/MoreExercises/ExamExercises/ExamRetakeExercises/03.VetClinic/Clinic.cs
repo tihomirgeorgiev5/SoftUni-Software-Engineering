bd2c6df96339acc2d292f74b43f4c2ab8d29dae6
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-12-11 21:24:00 +0100

    Added ExamPrep Problem

---
+++ CSharp-Advanced/MoreExercises/ExamExercises/ExamRetakeExercises/03.VetClinic/Clinic.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> pets;
+        public Clinic(int capacity)
+        {
+            this.Capacity = capacity;
+            this.pets = new List<Pet>();
+        }
+        public int Capacity { get; set; }
+
+        public int Count { get { return pets.Count; } }
+
+        public void Add(Pet pet)
+        {
+            if (pets.Count < Capacity)
+            {
+                pets.Add(pet);
+            }
+            
+        }
+        public bool Remove(string name)
+        {
+            var pet = pets.FirstOrDefault(p => p.Name == name);
+            if (pet == null)
+            {
+                return false;
+            }
+
+            pets.Remove(pet);
+            return true;
+
+        }
+        public Pet GetPet(string name, string owner)
+        {
+            var pet = pets.FirstOrDefault(p => p.Name == name && p.Owner == owner);
+
+            return pet;
+        }
+        public Pet GetOldestPet()
+        {
+            return pets.OrderByDescending(p => p.Age).FirstOrDefault();
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("The clinic has the following patients:");
+
+            foreach (var pet in pets)
+            {
+                sb.AppendLine($"Pet {pet.Name} with owner: {pet.Owner}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
