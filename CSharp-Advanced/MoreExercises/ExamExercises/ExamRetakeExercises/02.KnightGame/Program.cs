4c485b698467d80782acd90b36915edabd6422ec
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-12-13 02:00:18 +0100

    Added new ExamExercise

---
+++ CSharp-Advanced/MoreExercises/ExamExercises/ExamRetakeExercises/02.KnightGame/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Linq;
+
+namespace KnightGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] chessBoard = new char[n, n];
+
+            FillMatrix(chessBoard);
+
+            int countReplaced = 0;
+            int rowKiller = 0;
+            int colKiller = 0;
+
+            
+
+            while (true)
+            {
+                int maxAttacks = 0;
+                for (int row = 0; row < n; row++)
+                {
+                    for (int col = 0; col < n; col++)
+                    {
+                        char currentSymbol = chessBoard[row, col];
+                       
+                        int countattacks = 0;
+                        if (currentSymbol == 'K')
+                        {
+                            countattacks = GetAttacks(chessBoard, row, col, countattacks);
+
+                            if (countattacks > maxAttacks)
+                            {
+                                maxAttacks = countattacks;
+                                rowKiller = row;
+                                colKiller = col;
+                            }
+                            
+                        }
+                    }
+
+                }
+                if (maxAttacks > 0)
+                {
+                    chessBoard[rowKiller, colKiller] = '0';
+                    countReplaced++;
+                }
+                else
+                {
+                    Console.WriteLine(countReplaced);
+                    break;
+                }
+
+               
+
+            }
+           
+
+        }
+        
+
+
+        private static int GetAttacks(char[,] chessBoard, int row, int col, int countattacks)
+        {
+            if (IsInside(chessBoard, row - 2, col + 1) && chessBoard[row - 2, col + 1] == 'K')
+            {
+                countattacks++;
+            }
+
+            if (IsInside(chessBoard, row - 2, col - 1) && chessBoard[row - 2, col - 1] == 'K')
+            {
+                countattacks++;
+            }
+
+            if (IsInside(chessBoard, row + 1, col + 2) && chessBoard[row + 1, col + 2] == 'K')
+            {
+                countattacks++;
+            }
+            if (IsInside(chessBoard, row + 1, col - 2) && chessBoard[row + 1, col - 2] == 'K')
+            {
+                countattacks++;
+            }
+            if (IsInside(chessBoard, row - 1, col + 2) && chessBoard[row - 1, col + 2] == 'K')
+            {
+                countattacks++;
+            }
+            if (IsInside(chessBoard, row - 1, col - 2) && chessBoard[row - 1, col - 2] == 'K')
+            {
+                countattacks++;
+            }
+            if (IsInside(chessBoard, row + 2, col - 1) && chessBoard[row + 2, col - 1] == 'K')
+            {
+                countattacks++;
+            }
+            if (IsInside(chessBoard, row + 2, col + 1) && chessBoard[row + 2, col + 1] == 'K')
+            {
+                countattacks++;
+            }
+
+            return countattacks;
+        }
+
+        private static void PrintMatrix(char[,]chessBoard)
+        {
+            for (int row = 0; row < chessBoard
+                .GetLength(0); row++)
+            {
+                for (int col = 0; col < chessBoard.GetLength(1); col++)
+                {
+                    Console.Write(chessBoard[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static void FillMatrix(char[,] chessBoard)
+        {
+            for (int row = 0; row < chessBoard.GetLength(0); row++)
+            {
+                char[] currentRow = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < chessBoard.GetLength(1); col++)
+                {
+                    chessBoard[row, col] = currentRow[col];
+                }
+            }
+        }
+
+        public static bool IsPositionValid(int row, int col, int rows, int cols)
+        {
+            if (row < 0 || row >= rows)
+            {
+                return false;
+            }
+            if (col < 0 || col >= cols)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        private static bool IsInside (char[,] chessBoard, int row, int col)
+        {
+            return row >= 0 && row < chessBoard.GetLength(0) && col >= 0 && col < chessBoard.GetLength(1);
+        }
+    }
+}
