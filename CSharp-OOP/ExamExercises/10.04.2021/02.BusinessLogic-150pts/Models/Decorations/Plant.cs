1483b38ca68f3e43e1874b02ea46c63d4a6aafa4
Tihomir Georgiev <georgiev.tm@abv.bg>
2021-12-18 20:10:06 +0100

    Added second Problem of ExamRetake successfull solution in judge

---
+++ CSharp-OOP/ExamExercises/10.04.2021/02.BusinessLogic-150pts/Models/Decorations/Plant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaShop.Models.Decorations
+{
+    public class Plant : Decoration
+    {
+        //Has 5 comfort and its price is 10.
+        private const int PlantComfort = 5;
+        private const decimal PlantPrice = 10;
+        public Plant() : base(PlantComfort,PlantPrice)
+        {
+        }
+    }
+}
