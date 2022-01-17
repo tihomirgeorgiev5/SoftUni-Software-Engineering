28a73ba06458c8a81822cb434deecafbb571cd47
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-01-17 19:30:51 +0100

    added new exercise to async await

---
+++ CSharp-Web-Basics/Labs/WebServer-AsynchronousProcessing/DownloadInternetSiteCondition/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Stamo_sAsyncAwait
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            string url = "https://softuni.bg";
+
+            using (HttpClient client = new HttpClient())
+            {
+                var result = await client.GetAsync(url);
+
+                if (result.IsSuccessStatusCode)
+                {
+                    string content = await result.Content.ReadAsStringAsync();
+                    Console.WriteLine(content);
+                }
+            }
+        }
+    }
+}
