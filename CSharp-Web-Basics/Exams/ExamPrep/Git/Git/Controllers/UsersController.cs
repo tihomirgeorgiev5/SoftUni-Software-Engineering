152792531901821f78303eb92eb5c79f425540da
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-02-15 01:17:32 +0100

    Now we can go to Register()

---
+++ CSharp-Web-Basics/Exams/ExamPrep/Git/Git/Controllers/UsersController.cs
@@ -0,0 +1,11 @@
+﻿using MyWebServer.Controllers;
+using MyWebServer.Http;
+
+namespace Git.Controllers
+{
+    public class UsersController : Controller
+    {
+        public HttpResponse Register()
+            => View();
+    }
+}
