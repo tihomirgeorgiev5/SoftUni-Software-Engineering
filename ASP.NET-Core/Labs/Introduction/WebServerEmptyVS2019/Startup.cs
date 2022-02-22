1e10b376059e4a75a5273f5d1c6c785ad670cb7a
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-02-22 02:06:28 +0100

    Added elementary code in ASP

---
+++ ASP.NET-Core/Labs/Introduction/WebServerEmptyVS2019/Startup.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebServerEmptyVS2019
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+
+                endpoints.MapGet("/Cats", async context =>
+                {
+                    if (context.Request.Query.ContainsKey("name"))
+                    {
+                        context.Response.Headers.Add("My-Cat", "Sharo");
+                    }
+
+                    await context.Response.WriteAsync("Cats!");
+                });
+                    
+            });
+        }
+    }
+}
