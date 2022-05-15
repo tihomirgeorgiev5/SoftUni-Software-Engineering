f86e75825b01e019d339f11e64a95720797b8e1e
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-16 00:36:57 +0200

    added exercise StringLength

--- JS-Advanced/HomeWorks-And-Labs/01.SyntaxFunctionsAndStatements-Lab/stringLength/stringLength.js
+++ JS-Advanced/HomeWorks-And-Labs/01.SyntaxFunctionsAndStatements-Lab/stringLength/stringLength.js
@@ -11,3 +11,16 @@ function stringLength(str1, str2, str3) {
 
 }
 stringLength('chocolate', 'ice cream', 'cake')
+
+function stringLength(input1, input2, input3) {
+    let input1Length = input1.length;
+    let input2Length = input2.length;
+    let input3Length = input3.length;
+
+    let sumLength = input1Length + input2Length + input3Length;
+    let averageLength = Math.floor(sumLength / 3);
+    console.log(sumLength);
+    console.log(averageLength);
+
+}
+stringLength('pasta', '5', '22.3')
