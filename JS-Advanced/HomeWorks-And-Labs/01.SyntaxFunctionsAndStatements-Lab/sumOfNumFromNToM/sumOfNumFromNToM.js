92d0743ab04b7b64e57c3a47731c444586e54cfa
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-16 20:16:02 +0200

    added new exercise sumOfNumbers

--- JS-Advanced/HomeWorks-And-Labs/01.SyntaxFunctionsAndStatements-Lab/sumOfNumFromNToM/sumOfNumFromNToM.js
+++ JS-Advanced/HomeWorks-And-Labs/01.SyntaxFunctionsAndStatements-Lab/sumOfNumFromNToM/sumOfNumFromNToM.js
@@ -11,3 +11,16 @@ function solve(n, m) {
 
 }
 solve('1', '5')
+
+function sumOfNum(n, m) {
+    let num1 = Number(n);
+    let num2 = Number(m);
+    let result = 0;
+
+    for (let i = num1; i <= num2; i++) {
+        result += i;    
+    }
+    console.log(result);
+
+}
+sumOfNum('-8', '20')
