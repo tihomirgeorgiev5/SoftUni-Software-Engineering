21104ce52728cb73e1d3d5ca9f81ea8a418dc55f
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-17 20:57:01 +0200

    added new exercise daysOfWeek

--- JS-Advanced/HomeWorks-And-Labs/01.SyntaxFunctionsAndStatements-Lab/dayOfWeek/dayOfWeek.js
+++ JS-Advanced/HomeWorks-And-Labs/01.SyntaxFunctionsAndStatements-Lab/dayOfWeek/dayOfWeek.js
@@ -1,18 +1,53 @@
 function solve(input) {
-   let dayOfWeek = input;
+    let dayOfWeek = input;
     let result = 0;
 
     switch (dayOfWeek) {
-        case 'Monday': result = 1;break;
-        case 'Tuesday': result = 2;break;
-        case 'Wednesday': result = 3;break;
-        case 'Thursday': result = 4;break;
-        case 'Friday': result = 5;break;
-        case 'Saturday': result = 6;break;
-        case 'Sunday': result = 7;break;
-        default: result = 'error';break;
+        case 'Monday': result = 1; break;
+        case 'Tuesday': result = 2; break;
+        case 'Wednesday': result = 3; break;
+        case 'Thursday': result = 4; break;
+        case 'Friday': result = 5; break;
+        case 'Saturday': result = 6; break;
+        case 'Sunday': result = 7; break;
+        default: result = 'error'; break;
     }
     console.log(result);
 
 }
 solve('Invalid')
+
+function dayOfWeek(input) {
+    let result;
+
+    switch (input) {
+        case 'Monday':
+            result = 1;
+            break;
+        case 'Tuesday':
+            result = 2;
+            break;
+        case 'Wednesday':
+            result = 3;
+            break;
+        case 'Thursday':
+            result = 4;
+            break;
+        case 'Friday':
+            result = 5;
+            break;
+        case 'Saturday':
+            result = 6;
+            break;
+        case 'Sunday':
+            result = 7;
+            break;
+        default:
+            result = 'error';
+            break;
+    }
+    console.log(result)
+
+}
+
+dayOfWeek('Friday')
