020c88bf097077224a99cc0f97b49a2862f021d1
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-18 20:42:49 +0200

    added new exercise in lab => aggregateEllements

--- JS-Advanced/HomeWorks-And-Labs/01.SyntaxFunctionsAndStatements-Lab/aggregateEllements/aggregateEllements.js
+++ JS-Advanced/HomeWorks-And-Labs/01.SyntaxFunctionsAndStatements-Lab/aggregateEllements/aggregateEllements.js
@@ -6,13 +6,31 @@ function aggregateElements(elements) {
     for (let num of elements) {
         totalSum += num;
         inverseSum += 1 / num;
-        concatNum += num;   
+        concatNum += num;
     }
 
     console.log(totalSum);
     console.log(inverseSum);
     console.log(concatNum);
 
-    
+
 }
 aggregateElements([1, 2, 3])
+
+function aggElements(elements) {
+    let sum = 0;
+    let invSum = 0;
+    let concNum = '';
+
+    for (const num of elements) {
+        sum += num;
+        invSum += 1 / num;
+        concNum += num;
+    }
+
+    console.log(sum);
+    console.log(invSum);
+    console.log(concNum);
+
+}
+aggElements([2, 4, 8, 16])
