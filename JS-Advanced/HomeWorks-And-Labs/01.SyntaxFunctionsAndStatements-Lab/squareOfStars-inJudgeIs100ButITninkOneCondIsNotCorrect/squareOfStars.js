3e83b42a318aa423aa08b23c8b28c2fe15a70b9b
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-18 20:29:30 +0200

    Added new exercise in lab => squareStars

--- JS-Advanced/HomeWorks-And-Labs/01.SyntaxFunctionsAndStatements-Lab/squareOfStars-inJudgeIs100ButITninkOneCondIsNotCorrect/squareOfStars.js
+++ JS-Advanced/HomeWorks-And-Labs/01.SyntaxFunctionsAndStatements-Lab/squareOfStars-inJudgeIs100ButITninkOneCondIsNotCorrect/squareOfStars.js
@@ -1,12 +1,28 @@
-function size(stars) {
-    if(stars === '') {
+/*function size(stars) {
+    if (stars === '') {
         stars === 5;
-        
+
     } else {
-        for (i = 0; i < stars; i++)  console.log('* '.repeat(stars));
+        for (i = 0; i < stars; i++)
+            console.log('* '.repeat(stars));
 
     }
-    
-    
-  }
-size(5)
+
+
+}
+size(5)*/
+
+function squareOfstars(numOfStars) {
+    if (numOfStars === undefined) {
+        for (i = 0; i < 5; i++)
+            console.log('* '.repeat('5'));
+
+    } else {
+
+        for (i = 0; i < numOfStars; i++)
+            console.log('* '.repeat(numOfStars));
+
+    }
+
+}
+squareOfstars()
