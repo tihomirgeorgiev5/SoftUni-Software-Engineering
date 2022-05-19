562583b9a3016860e72f85dc00d27844b107f4be
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-19 02:32:12 +0200

    added new exercise => greatDividers

--- JS-Advanced/HomeWorks-And-Labs/01.SyntaxFunctionsAndStatements/02GreatestCommonDivisor/greatestCommonDivisor.js
+++ JS-Advanced/HomeWorks-And-Labs/01.SyntaxFunctionsAndStatements/02GreatestCommonDivisor/greatestCommonDivisor.js
@@ -10,3 +10,20 @@ function solve(x,y){
 
 console.log(solve(15, 5));
 console.log(solve(2154, 458));
+
+function gCD (num1, num2) {
+    let divident1 = Number(num1);
+    let divident2 = Number(num2);
+
+    while (divident1 !== divident2) {
+        if (divident1 > divident2) {
+            divident1-= divident2;
+        } else {
+            divident2-=divident1;
+        }
+    }
+    console.log(divident1);
+
+
+}
+gCD(2154, 458);
