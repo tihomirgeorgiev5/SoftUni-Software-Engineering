31d638dbc85f88c24ed1f76cbb0d249a2a1e0a74
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-19 02:18:45 +0200

    added fruit exercise

--- JS-Advanced/HomeWorks-And-Labs/01.SyntaxFunctionsAndStatements/01Fruit/fruit.js
+++ JS-Advanced/HomeWorks-And-Labs/01.SyntaxFunctionsAndStatements/01Fruit/fruit.js
@@ -9,3 +9,15 @@ function solve(string, num1, num2){
 }
 console.log(solve('orange', 2500, 1.80));
 
+function fruit(fruit, weight, money) {
+    let fruitType = fruit;
+    let weightInGr = weight;
+    let pricePerKg = money;
+    let weightInKg = weightInGr / 1000;
+    let needMoney = pricePerKg * weightInKg;
+
+    console.log(`I need $${needMoney.toFixed(2)} to buy ${weightInKg.toFixed(2)} kilograms ${fruitType}.`);
+
+}
+fruit('apple', 1563, 2.35)
+
