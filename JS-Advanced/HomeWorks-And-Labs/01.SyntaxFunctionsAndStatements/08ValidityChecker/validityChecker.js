655ed3ff49241f5ab4ebf9f29518c65d122ed7a9
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-20 22:16:22 +0200

    added new exercise => validityChecker

--- JS-Advanced/HomeWorks-And-Labs/01.SyntaxFunctionsAndStatements/08ValidityChecker/validityChecker.js
+++ JS-Advanced/HomeWorks-And-Labs/01.SyntaxFunctionsAndStatements/08ValidityChecker/validityChecker.js
@@ -1,4 +1,4 @@
-function solve(x1, y1, x2, y2){ 
+/*function solve(x1, y1, x2, y2){ 
     function distance(x1, y1, x2, y2) {
         let distH = x2 - x1;
         let distY = y2 - y1;
@@ -24,4 +24,33 @@ function solve(x1, y1, x2, y2){
     }
 
 }
-solve(2, 1, 1, 1);
+solve(2, 1, 1, 1);*/
+
+function validityChecker(x1, y1, x2, y2) {
+    function distance(x1, y1, x2, y2) {
+        return Math.sqrt(((x2 - x1)**2) + ((y2 - y1)**2));
+    }
+
+    if (Number.isInteger(distance(x1, y1, 0, 0))) {
+        console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
+        
+    }
+
+    if (Number.isInteger(distance(x2, y2, 0, 0))) {
+        console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
+    }
+
+    if (Number.isInteger(distance(x1, y1, x2, y2))) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+    }
+    
+}
+validityChecker(2, 1, 1, 1);
+
+
