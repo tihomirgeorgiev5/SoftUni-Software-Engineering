3bd807c9153ac81bdf85f217b2522135b21ff914
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-20 14:50:19 +0200

    added new exercise => sameNumbers

--- JS-Advanced/HomeWorks-And-Labs/01.SyntaxFunctionsAndStatements/03SameNumbers/sameNumbers.js
+++ JS-Advanced/HomeWorks-And-Labs/01.SyntaxFunctionsAndStatements/03SameNumbers/sameNumbers.js
@@ -1,4 +1,4 @@
-function solve(num){ 
+/*function solve(num){ 
     let string = String(num);
     let sum = 0;
     let isSame;
@@ -11,4 +11,25 @@ function solve(num){
    console.log(isSame)
    console.log(sum)
 }
-solve(2222222);
+solve(2222222);*/
+
+function sameNumbers(input) {
+    let inputString = String(input);
+    let isSame = true;
+    let digitSum = 0;
+    let inputLength = inputString.length;
+    let digitToCompare = Number(inputString[0]);
+
+
+    for (let index = 0; index < inputLength; index++) {
+        digitSum += Number(inputString[index]);
+        
+        if (Number(inputString[index]) !== digitToCompare) {
+            isSame = false;
+        } 
+    }
+    console.log(isSame);
+    console.log(digitSum);
+
+}
+sameNumbers(1234);
