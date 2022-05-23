1e114aef9ed0a0bc5486c13b2c466d52dcb09629
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-23 19:34:40 +0200

    added new exercise in lab => negativePositiveNum

--- JS-Advanced/HomeWorks-And-Labs/02.ArraysAndNestedArrays-Lab/04.NegativePositiveNumbers/04negativePositiveNumbers.js
+++ JS-Advanced/HomeWorks-And-Labs/02.ArraysAndNestedArrays-Lab/04.NegativePositiveNumbers/04negativePositiveNumbers.js
@@ -1,4 +1,4 @@
-function solve(arr){ 
+/*function solve(arr){ 
     let newArr = [];
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] < 0) {
@@ -19,4 +19,22 @@ function solve(arr){
     }
 
 }
-solve([7, -2, 8, 9]);
+solve([7, -2, 8, 9]);*/
+
+function posNegNum(inputArr) {
+    const result = [];
+
+    for (const num of inputArr) {
+        if (num < 0) {
+            result.unshift(num);
+        } else {
+            result.push(num);
+            
+        }
+    }
+    console.log(result.join('\n'));
+
+}
+posNegNum([7, -2, 8, 9]);
+posNegNum([3, -2, 0, -1]);
+
