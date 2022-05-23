c129397156aefaf7fe5d43108a0b2c5ffc7d5e2c
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-23 19:51:47 +0200

    added new exercise in lab => biggestHalf

--- JS-Advanced/HomeWorks-And-Labs/02.ArraysAndNestedArrays-Lab/06.BiggerHalf/06biggerHalf.js
+++ JS-Advanced/HomeWorks-And-Labs/02.ArraysAndNestedArrays-Lab/06.BiggerHalf/06biggerHalf.js
@@ -1,5 +1,5 @@
 
-function solve(arr){ 
+/*function solve(arr){ 
     arr.sort((a,b)=> a - b);
     let newArr = [];
     let point = 0;
@@ -14,5 +14,20 @@ function solve(arr){
     
     return newArr;
 }
-console.log(solve([4, 7, 2, 5]))
+console.log(solve([4, 7, 2, 5]))*/
+
+function biggestHalf(arr) {
+    arr.sort((a, b) => a - b);
+    const result = [];
+
+    for (let index = Math.floor(arr.length / 2); index < arr.length; index++) {
+        
+        result.push(arr[index]);
+    }
+    return result;
+
+}
+console.log(biggestHalf([4, 7, 2, 5]));
+console.log(biggestHalf([3, 19, 14, 7, 2, 19, 6]));
+
 
