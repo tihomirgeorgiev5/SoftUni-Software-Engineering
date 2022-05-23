8d37bd2fa6e2fa26fc1b5b4288dc5d25b2b797b2
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-23 20:31:27 +0200

    added new exercise of lab => pieceOfPie

--- JS-Advanced/HomeWorks-And-Labs/02.ArraysAndNestedArrays-Lab/07.PieceOfPie/07pieceOfPie.js
+++ JS-Advanced/HomeWorks-And-Labs/02.ArraysAndNestedArrays-Lab/07.PieceOfPie/07pieceOfPie.js
@@ -1,4 +1,4 @@
-function solve(arrPies, firstPie, lastPie){ 
+/*function solve(arrPies, firstPie, lastPie){ 
     let firstIndexPie = arrPies.indexOf(firstPie);
     let lastIndexPie = arrPies.indexOf(lastPie);
 
@@ -11,4 +11,27 @@ console.log(solve(['Pumpkin Pie',
 'Lemon Meringue Pie',
 'Sugar Cream Pie'],
 'Key Lime Pie',
-'Lemon Meringue Pie'))
+'Lemon Meringue Pie'))*/
+
+function piecePie(input, start, end) {
+    const startIndex = input.indexOf(start);
+    const endIndex = input.indexOf(end) + 1;
+
+    return input.slice(startIndex, endIndex);
+
+}
+console.log(piecePie['Pumpkin Pie',
+'Key Lime Pie',
+'Cherry Pie',
+'Lemon Meringue Pie',
+'Sugar Cream Pie'],
+'Key Lime Pie',
+'Lemon Meringue Pie');
+console.log(piecePie(['Apple Crisp',
+'Mississippi Mud Pie',
+'Pot Pie',
+'Steak and Cheese Pie',
+'Butter Chicken Pie',
+'Smoked Fish Pie'],
+'Pot Pie',
+'Smoked Fish Pie'))
