7b4f5abebb88be300838c108c31db787378afb98
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-24 17:40:37 +0200

    added new exercise in lab => processOddPositions

--- JS-Advanced/HomeWorks-And-Labs/02.ArraysAndNestedArrays-Lab/08.ProcessOddPositions/08processOddPositions.js
+++ JS-Advanced/HomeWorks-And-Labs/02.ArraysAndNestedArrays-Lab/08.ProcessOddPositions/08processOddPositions.js
@@ -6,3 +6,15 @@ return arr.filter((a, i) => i % 2 !== 0)
 
 }
 console.log(solve([10, 15, 20, 25]))
+
+function processOddPosition (input) {
+    const oddPositions = input.filter((x,i) => i % 2 != 0);
+    const doubled = oddPositions.map(x => x*2);
+    doubled.reverse();
+    return doubled.join(' ');
+
+
+
+}
+console.log(processOddPosition([10, 15, 20, 25]));
+console.log(processOddPosition([3, 0, 10, 4, 7, 3])); 
