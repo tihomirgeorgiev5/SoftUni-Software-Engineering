a4d9b9243cad3eddfed7ab3b8cad81f0b80f9130
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-26 00:30:17 +0200

    added new exercise => rotateArray

--- JS-Advanced/HomeWorks-And-Labs/02.ArraysAndNestedArrays/04.RotateArray/04rotateArray.js
+++ JS-Advanced/HomeWorks-And-Labs/02.ArraysAndNestedArrays/04.RotateArray/04rotateArray.js
@@ -1,4 +1,4 @@
-function solve(arr, num){ 
+/*function solve(arr, num){ 
     let realRotate = num % arr.length;
     for (let index = 0; index < realRotate; index++) {
         let ex = arr.pop();
@@ -12,4 +12,23 @@ console.log(['Banana',
 'Orange', 
 'Coconut', 
 'Apple'], 
-15);
+15);*/
+
+function rotateArr(arr, rotation) {
+
+    for (let index = 0; index < rotation; index++) {
+        const lastEl = arr.pop();
+        arr.unshift(lastEl);
+    }
+    return arr.join(' '); 
+}
+console.log(rotateArr(['1', 
+'2', 
+'3', 
+'4'], 
+2));
+console.log(rotateArr(['Banana', 
+'Orange', 
+'Coconut', 
+'Apple'], 
+15));
