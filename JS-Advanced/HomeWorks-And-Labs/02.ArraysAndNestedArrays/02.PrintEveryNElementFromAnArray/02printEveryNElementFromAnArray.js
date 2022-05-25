a32dcc863210ca7b26a170c41d166b5ca3bfaa2c
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-25 23:13:14 +0200

    added new exercise => printEveryElFromArr

--- JS-Advanced/HomeWorks-And-Labs/02.ArraysAndNestedArrays/02.PrintEveryNElementFromAnArray/02printEveryNElementFromAnArray.js
+++ JS-Advanced/HomeWorks-And-Labs/02.ArraysAndNestedArrays/02.PrintEveryNElementFromAnArray/02printEveryNElementFromAnArray.js
@@ -1,4 +1,4 @@
-function solve(arr, step){ 
+/*function solve(arr, step){ 
     let newArr = [];
     for (let i = 0; i < arr.length; i++) {
         if (i % Number(step) === 0){
@@ -16,4 +16,32 @@ console.log(solve(['5',
 '31', 
 '4', 
 '20'], 
-2))
+2))*/
+
+function printElFromArr(arr, step){
+    let newArr = [];
+    for (let index = 0; index < arr.length; index+=step) {
+        newArr.push(arr[index]);    
+    }
+
+    return newArr;
+
+}
+console.log(printElFromArr(['5', 
+'20', 
+'31', 
+'4', 
+'20'], 
+2));
+console.log(printElFromArr(['dsa',
+'asd', 
+'test', 
+'tset'], 
+2
+));
+console.log(printElFromArr(['1', 
+'2',
+'3', 
+'4', 
+'5'], 
+6));
