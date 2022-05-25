b5e61e09943b8680fc5b5b92f5b3f63dfa759b80
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-26 01:28:36 +0200

    added new exercise => sortedNumbers

--- JS-Advanced/HomeWorks-And-Labs/02.ArraysAndNestedArrays/07.SortingNumbers/07sortingNumbers.js
+++ JS-Advanced/HomeWorks-And-Labs/02.ArraysAndNestedArrays/07.SortingNumbers/07sortingNumbers.js
@@ -1,4 +1,4 @@
-function solve(arr) {
+/*function solve(arr) {
     arr.sort((a, b) => a - b);
     
     let newArr = [];
@@ -13,4 +13,16 @@ function solve(arr) {
     
 
 }
-console.log(solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]))
+console.log(solve([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]))*/
+
+function sortingNumbers(arr) {
+    let sortedArr = arr.sort((a,b) => a-b);
+    
+     const newArr = [];
+    while (arr.length !== 0) {
+        newArr.push(sortedArr.shift());
+        newArr.push(sortedArr.pop());
+    }
+    return newArr;
+}
+console.log(sortingNumbers([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]));
