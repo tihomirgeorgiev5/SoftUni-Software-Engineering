f0c7b8cd69db1f9601749bfdda48203783bc497d
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-26 01:05:11 +0200

    added new exercise => listedNames

--- JS-Advanced/HomeWorks-And-Labs/02.ArraysAndNestedArrays/06.ListOfNames/06listOfNames.js
+++ JS-Advanced/HomeWorks-And-Labs/02.ArraysAndNestedArrays/06.ListOfNames/06listOfNames.js
@@ -1,4 +1,4 @@
-function solve(arr){ 
+/*function solve(arr){ 
     arr.sort((a, b) => a.localeCompare(b));
     
     for (i = 0; i < arr.length; i++) {
@@ -8,4 +8,18 @@ function solve(arr){
     }
 
 }
-solve(["John", "Bob", "Christina", "Ema"]);
+solve(["John", "Bob", "Christina", "Ema"]);*/
+
+function listNames(arr){
+   
+    arr.sort((a,b) => a.localeCompare(b));
+    
+
+    for (let index = 0; index < arr.length; index++) {
+       
+        console.log(`${index + 1}.${arr[index]}`);
+        
+    }
+
+}
+listNames(["John", "Bob", "Christina", "Ema"]);
