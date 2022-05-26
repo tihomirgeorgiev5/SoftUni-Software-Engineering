d0946e0778f552fe7f1e0b0db2156f557c1684a2
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-26 10:45:44 +0200

    added new exercise in lab => smallestTwoNums

--- JS-Advanced/HomeWorks-And-Labs/02.ArraysAndNestedArrays-Lab/05.SmallestTwoNumbers/05smallestTwoNumbers.js
+++ JS-Advanced/HomeWorks-And-Labs/02.ArraysAndNestedArrays-Lab/05.SmallestTwoNumbers/05smallestTwoNumbers.js
@@ -1,4 +1,4 @@
-function solve(arr){ 
+/*function solve(arr){ 
     arr.sort((a,b)=>a-b);
     
     return (arr[0] + ' ' + arr[1]);
@@ -6,4 +6,15 @@ function solve(arr){
 
 
 }
-console.log(solve([30, 15, 50, 5]))
+console.log(solve([30, 15, 50, 5]))*/
+
+function smallestTwoNum(arr) {
+    let newArr = [];
+    arr.sort((a,b) => a-b);
+    arr.splice(2);
+    console.log(arr);
+
+}
+smallestTwoNum([30, 15, 50, 5]);
+console.log('----');
+smallestTwoNum([3, 0, 10, 4, 7, 3]);
