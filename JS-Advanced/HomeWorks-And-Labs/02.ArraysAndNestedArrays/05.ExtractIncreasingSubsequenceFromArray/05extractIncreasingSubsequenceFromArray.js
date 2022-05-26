644a5d7dd781c65a2ac0913378eb8356153f6621
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-26 09:45:20 +0200

    added new exercise => extractIncreasingSubseq...

--- JS-Advanced/HomeWorks-And-Labs/02.ArraysAndNestedArrays/05.ExtractIncreasingSubsequenceFromArray/05extractIncreasingSubsequenceFromArray.js
+++ JS-Advanced/HomeWorks-And-Labs/02.ArraysAndNestedArrays/05.ExtractIncreasingSubsequenceFromArray/05extractIncreasingSubsequenceFromArray.js
@@ -1,4 +1,4 @@
-function solve(arr) {
+/*function solve(arr) {
     let maxNum = Number.MIN_SAFE_INTEGER;
 
     let result = arr.reduce((a, x) => {
@@ -16,6 +16,43 @@ function solve(arr) {
     return result;
 
 }
+console.log(solve([20,
+    3,
+    2,
+    15,
+    6,
+    1]));*/
+
+function solve(arr) {
+    let sum = 0;
+
+    let result = arr.reduce((a,x) => {
+        if (x >= sum) {
+            sum = x;
+            a.push(x);
+        }
+        return a;
+    }, []);
+
+    return result;
+    
+
+}
+console.log(solve([1,
+    3,
+    8,
+    4,
+    10,
+    12,
+    3,
+    2,
+    24]));
+console.log('-----');
+console.log(solve([1,
+    2,
+    3,
+    4]));
+console.log('-----');
 console.log(solve([20,
     3,
     2,
