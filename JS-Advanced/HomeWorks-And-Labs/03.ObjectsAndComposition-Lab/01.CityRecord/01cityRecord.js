ec6b9487ca9bfd1baa75989ac73f845ff34f71ec
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-26 23:26:10 +0200

    added first exercise from lab => cityRecord

---
+++ JS-Advanced/HomeWorks-And-Labs/03.ObjectsAndComposition-Lab/01.CityRecord/01cityRecord.js
@@ -0,0 +1,31 @@
+/*function solve(name, population, treasury){ 
+    let town = {
+        name: name,
+        population: population,
+        treasury: treasury,
+    }
+
+    return town;
+
+}
+console.log(solve('Tortuga',
+7000,
+15000));*/
+
+function cityRecord(name, population, treasury) {
+const result = {
+    name: name,
+    population: population,
+    treasury:treasury,
+};
+
+return result;
+}
+console.log(cityRecord('Tortuga',
+7000,
+15000));
+console.log(cityRecord('Santo Domingo',
+12000,
+23500));
+
+
