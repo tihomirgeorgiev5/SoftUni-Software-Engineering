7a3e828dd4d1a3559e85c74c15fff81224bc2349
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-29 09:23:13 +0200

    added new solution of carFactory

--- JS-Advanced/HomeWorks-And-Labs/03.ObjectsAndComposition/03carFactory.js
+++ JS-Advanced/HomeWorks-And-Labs/03.ObjectsAndComposition/03carFactory.js
@@ -1,4 +1,4 @@
-function carFactory(input) {
+/*function carFactory(input) {
 
     let result = {};
     let engine = {};
@@ -32,17 +32,70 @@ function carFactory(input) {
     
      return result;
    
+}*/
+function carFactory(input) {
+    const {
+        model,
+        power,
+        carriage,
+        type,
+        color,
+        wheelsize,
+    } = input;
+    function getEngine(power) {
+        const engines = [
+            { power: 90, volume: 1800 },
+            { power: 120, volume: 2400 },
+            { power: 200, volume: 3500 },
+        ].sort((a, b) => a.power - b.power);
+        let result;
+        /* for (let i = 0; i < engines.length; i++) {
+             if (engines[i].power >= power) {
+                 result = engines[i];
+                 break;
+             }
+             
+         }*/
+        return engines.find(el => el.power >= power);
+    }
+
+    function getCarriage(type, color) {
+        return {
+            type : carriage,
+            color,
+        };
+    }
+
+    function getWheels(wheelsize) {
+        let wheel =
+            wheelsize % 2 == 0 
+            ? wheelsize - 1 
+            : wheelsize;
+
+            return [wheel, wheel, wheel, wheel];
+    }
+    return {
+        model,
+        engine: getEngine(power),
+        carriage: getCarriage(type,color),
+        wheels: getWheels(wheelsize),
+    }
 }
-console.log(carFactory({ model: 'VW Golf II',
-power: 90,
-color: 'blue',
-carriage: 'hatchback',
-wheelsize: 14 })); 
+
+console.log(carFactory({
+    model: 'VW Golf II',
+    power: 90,
+    color: 'blue',
+    carriage: 'hatchback',
+    wheelsize: 14
+}));
 
 console.log('-----');
 
-console.log(carFactory({ model: 'Opel Vectra',
-power: 110,
-color: 'grey',
-carriage: 'coupe',
-wheelsize: 17 }));
+console.log(carFactory({
+    model: 'Opel Vectra',
+    power: 110,
+    color: 'grey',
+    carriage: 'coupe',
+    wheelsize: 17
+}));
