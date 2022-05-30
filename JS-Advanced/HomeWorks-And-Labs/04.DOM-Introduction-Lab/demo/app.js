45567f055e8fbdcd08b76490c20b148043454471
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-30 22:55:18 +0200

    added first exercise in lab

--- JS-Advanced/HomeWorks-And-Labs/04.DOM-Introduction-Lab/demo/app.js
+++ JS-Advanced/HomeWorks-And-Labs/04.DOM-Introduction-Lab/demo/app.js
@@ -1,3 +1,7 @@
-function sum(a, b) {
-    console.log(a + b);
+function edit(element, match, replacer) {
+    const text = element.textContent;
+
+    const pattern = new RegExp(match, 'g');
+    const result = text.replace(pattern, replacer);
+    element.textContent = result;
 }
