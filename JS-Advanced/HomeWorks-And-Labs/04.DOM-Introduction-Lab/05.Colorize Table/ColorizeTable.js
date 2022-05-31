36c427cf4a89cf10c58d13c0596cf0ca68a08706
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-05-31 19:48:58 +0200

    added new exercise in lab => colorizeTable

--- JS-Advanced/HomeWorks-And-Labs/04.DOM-Introduction-Lab/05.Colorize Table/ColorizeTable.js
+++ JS-Advanced/HomeWorks-And-Labs/04.DOM-Introduction-Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,8 @@
 function colorize() {
-    // TODO
+   const rows = document.querySelectorAll('tr');
+
+   for (let i = 1; i < rows.length; i+=2) {
+       rows[i].style.background = 'teal';
+       
+   }
 }
