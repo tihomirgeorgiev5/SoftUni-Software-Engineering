88a9f5dfca3dfe50abb3eab8f3995c527bad987f
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-06-01 20:47:44 +0200

    added new exercise => accordion

--- JS-Advanced/HomeWorks-And-Labs/04.DOM-Introduction/03. Accordion/accordion.js
+++ JS-Advanced/HomeWorks-And-Labs/04.DOM-Introduction/03. Accordion/accordion.js
@@ -1,3 +1,14 @@
 function toggle() {
-    console.log('TODO:...');
+   const button = document.querySelector('.button');
+
+   const text = document.querySelector('#extra');
+
+   if (button.textContent === 'More') {
+       button.textContent = 'Less';
+       text.style.display = 'block';
+   } else {
+       button.textContent = 'More';
+       text.style.display = 'none';
+   }
+
 }
