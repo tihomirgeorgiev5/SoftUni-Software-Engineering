f5f1e2f546db4cb0c790c22c13057655e36446d8
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-06-01 03:16:11 +0200

    added new exercise => camelCaseAndPascalCase

--- JS-Advanced/HomeWorks-And-Labs/04.DOM-Introduction/02. Pascal or Camel Case/solution.js
+++ JS-Advanced/HomeWorks-And-Labs/04.DOM-Introduction/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,36 @@
 function solve() {
-  //TODO...
+  let text = document.getElementById('text').value;
+  let convention = document.getElementById('naming-convention').value;
+  let result = '';
+
+
+  if (convention === 'Camel Case') {
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] === ' ') {
+        result += text[i + 1].toUpperCase();
+        i++;
+      } else {
+        result += text[i].toLowerCase();
+      }
+
+    }
+
+  } else if (convention === 'Pascal Case') {
+    result += text[0].toUpperCase();
+    for (let j = 1; j < text.length; j++) {
+      if (text[j] === ' ') {
+        result += text[j + 1].toUpperCase();
+        j++;
+      } else {
+        result += text[j].toLowerCase();
+      }
+
+    }
+  } else {
+    result += 'Error!'
+  };
+
+  document.getElementById('result').textContent = result;
+
+
 }
