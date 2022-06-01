37d2ee2e4c282f8440923155c1db7034c1b4355d
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-06-01 02:34:08 +0200

    added first exercise in lab => subtract

--- JS-Advanced/HomeWorks-And-Labs/04.DOM-Introduction/01. Subtraction/subtract.js
+++ JS-Advanced/HomeWorks-And-Labs/04.DOM-Introduction/01. Subtraction/subtract.js
@@ -1,3 +1,15 @@
 function subtract() {
-    console.log('TODO:...');
+    let firstNum = document.getElementById('firstNumber');
+    let secondNum = document.getElementById('secondNumber');
+
+    const firstParsedNum = Number(firstNum.value);
+    const secondParsedNum = Number(secondNum.value);
+
+    let subtract = firstParsedNum - secondParsedNum;
+
+    document.getElementById('result').textContent = subtract;
+
+    
+
+    console.log(subtract);
 }
