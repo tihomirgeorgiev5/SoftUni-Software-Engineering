b867c758cd45f6910468b51c11c02ec324e3f3b7
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-06-02 17:47:35 +0200

    added new exercise => search

--- JS-Advanced/HomeWorks-And-Labs/04.DOM-Introduction/04. Search in List/search.js
+++ JS-Advanced/HomeWorks-And-Labs/04.DOM-Introduction/04. Search in List/search.js
@@ -1,3 +1,22 @@
 function search() {
-   // TODO
+   const searchText = document.getElementById('searchText').value;
+   const result = document.getElementById('result');
+
+   const towns = Array.from(document.querySelectorAll('#towns li'));
+   let count = 0;
+ 
+
+   for (const town of towns) {
+      if (town.textContent.includes(searchText)) {
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration = 'underline';
+         count ++;
+      } else {
+         town.style.fontWeight = 'normal';
+         town.style.textDecoration = 'none';
+      }
+   }
+
+   result.textContent = `${count} matches found`;
+    
 }
