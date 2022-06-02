c8de8f0b349556437573330db1efd9f0b65c8d2b
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-06-02 23:52:51 +0200

    added new exercise => tableSearch

--- JS-Advanced/HomeWorks-And-Labs/04.DOM-Introduction/05. Table - Search Engine/solution.js
+++ JS-Advanced/HomeWorks-And-Labs/04.DOM-Introduction/05. Table - Search Engine/solution.js
@@ -1,8 +1,16 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
+const input = document.getElementById('searchField');
+const rows = document.querySelectorAll('tbody tr');
+ 
 
-   function onClick() {
-      //   TODO:
+function onClick() {
+     for (const row of rows) {
+        row.classList.remove('select');
+        if (input.value !== '' && row.innerHTML.includes(input.value)) {   
 
+         row.className = 'select';
+      }
+     }
    }
 }
