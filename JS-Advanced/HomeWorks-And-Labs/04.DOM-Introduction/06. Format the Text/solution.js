3ab94d00c297c43f08fe990ff1be62aa5907b43e
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-06-03 16:36:15 +0200

    added new exercise => formatText

--- JS-Advanced/HomeWorks-And-Labs/04.DOM-Introduction/06. Format the Text/solution.js
+++ JS-Advanced/HomeWorks-And-Labs/04.DOM-Introduction/06. Format the Text/solution.js
@@ -1,3 +1,12 @@
 function solve() {
-  //TODO
+  const input = document.getElementById('input').value;
+  const textArr = input.split('.').filter(s => s !== '');
+  const result = document.getElementById('output');
+
+  while (textArr.length > 0) {
+    const text = textArr.splice(0, 3).join('. ')+ '.';
+    const p = document.createElement('p');
+    p.textContent = text;
+    result.appendChild(p);
+  }
 }
