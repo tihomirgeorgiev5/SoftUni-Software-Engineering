792126e8d260a6e2e4301c7a241573962c212f32
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-06-04 22:51:20 +0200

    added new exercise in lab => Add-delete

--- JS-Advanced/HomeWorks-And-Labs/05.DOM-ManipulationAndEvents-Lab/03. Add-Delete/app.js
+++ JS-Advanced/HomeWorks-And-Labs/05.DOM-ManipulationAndEvents-Lab/03. Add-Delete/app.js
@@ -1,3 +1,26 @@
 function addItem() {
-    //TODO...
+    //select input field and read its value
+
+    const input = document.getElementById('newItemText');
+    // create <li> element and assign input value to element text content
+    const liElement = document.createElement('li');
+    liElement.textContent = input.value;
+
+    // create [Delete] anchor 
+    const deleteBtn = document.createElement('a');
+    deleteBtn.textContent = '[Delete]';
+    deleteBtn.href = '#';
+    liElement.appendChild(deleteBtn);
+    deleteBtn.addEventListener('click', onDelete)
+
+    // select <ul> and append new element
+    document.getElementById('items').appendChild(liElement);
+    // select input field and clear contents (value)
+    input.value = '';
+    function onDelete(event) {
+        event.target.parentElement.remove();
+    } 
 }
+//function onDelete(event) {
+    //event.target.parentElement.remove();
+//} 
