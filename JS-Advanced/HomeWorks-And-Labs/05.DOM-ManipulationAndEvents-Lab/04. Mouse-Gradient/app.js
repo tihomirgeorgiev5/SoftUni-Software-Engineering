271f2ea1f8bb43a175fb0365251a82cae61ab672
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-06-05 08:29:29 +0200

    added new exercise in lab => mouseGradient

--- JS-Advanced/HomeWorks-And-Labs/05.DOM-ManipulationAndEvents-Lab/04. Mouse-Gradient/app.js
+++ JS-Advanced/HomeWorks-And-Labs/05.DOM-ManipulationAndEvents-Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,11 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+   const gradient = document.getElementById('gradient');
+   gradient.addEventListener('mousemove', onMouseOver);
+   const result = document.getElementById('result');
+
+
+   function onMouseOver(ev) {
+       result.textContent = Math.floor(ev.offsetX / gradient.clientWidth * 100) + '%';
+
+   }
 }
