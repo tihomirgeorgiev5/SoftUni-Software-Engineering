91acbff950cc680b61745e728fb4ecd8484df33b
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-06-06 13:12:12 +0200

    added new exercise => lockedProfile

--- JS-Advanced/HomeWorks-And-Labs/05.DOM-ManipulationAndEvents/03. Locked-Profile/app.js
+++ JS-Advanced/HomeWorks-And-Labs/05.DOM-ManipulationAndEvents/03. Locked-Profile/app.js
@@ -1,3 +1,25 @@
 function lockedProfile() {
-    console.log('TODO...')
+    Array.from(document.querySelectorAll('.profile button'))
+        .forEach(b => b.addEventListener('click', onClick));
+
+    function onClick(ev) {
+        const profile = ev.target.parentElement;
+        const isActive = profile.querySelector('input[value="unlock"]').checked;
+        if (isActive) {
+            let info = Array.from(profile.querySelectorAll('div'))
+                .find(p => p.id.includes('HiddenFields'));
+
+            if (ev.target.textContent === 'Show more') {
+                ev.target.textContent = 'Hide it';
+                info.style.display = 'block';
+            } else {
+                ev.target.textContent = 'Show more';
+                info.style.display = 'none';
+            }
+
+
+
+        }
+    }
+
 }
