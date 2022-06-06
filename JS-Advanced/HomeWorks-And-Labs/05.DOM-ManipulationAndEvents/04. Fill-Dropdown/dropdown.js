0e2d3d5309ec251108bf6787bacf95dd63eecdbd
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-06-06 16:10:21 +0200

    added new exercise => dropDown

--- JS-Advanced/HomeWorks-And-Labs/05.DOM-ManipulationAndEvents/04. Fill-Dropdown/dropdown.js
+++ JS-Advanced/HomeWorks-And-Labs/05.DOM-ManipulationAndEvents/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,18 @@
 function addItem() {
-    console.log('TODO:...');
+    const newText = document.getElementById('newItemText').value;
+    const newValue = document.getElementById('newItemValue').value;
+
+    const option = document.createElement('option');
+    option.text = newText,
+        option.value = newValue;
+
+    const dropDownMenu = document.getElementById('menu');
+
+    if (newText !== '' && newValue !== '') {
+        dropDownMenu.appendChild(option);
+    }
+
+    document.getElementById('newItemText').value = '';
+    document.getElementById('newItemValue').value = '';
+
 }
