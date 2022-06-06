baca45beadc23172d665293f0220a5b76ccae027
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-06-06 09:26:51 +0200

    added new exercise => sections

--- JS-Advanced/HomeWorks-And-Labs/05.DOM-ManipulationAndEvents/01. Sections/script.js
+++ JS-Advanced/HomeWorks-And-Labs/05.DOM-ManipulationAndEvents/01. Sections/script.js
@@ -1,3 +1,20 @@
 function create(words) {
-   console.log('TODO:...');
+   const result = document.getElementById('content');
+
+
+   for (const word of words) {
+      const p = document.createElement('p');
+      const div = document.createElement('div');
+      const text = document.createTextNode(word);
+      p.appendChild(text);
+      p.style.display = 'none';
+      div.appendChild(p);
+      div.addEventListener('click', onClick);
+      result.appendChild(div);
+   }
+   
+   function onClick(ev) {
+     ev.target.children[0].style.display = 'block';
+   }
+  
 }
