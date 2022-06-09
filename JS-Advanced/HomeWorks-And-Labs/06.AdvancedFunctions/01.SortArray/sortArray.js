50c812cacf90362e0b422335cc4d97200c59d0da
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-06-10 00:04:22 +0200

    added first exercise in AdvFunc

---
+++ JS-Advanced/HomeWorks-And-Labs/06.AdvancedFunctions/01.SortArray/sortArray.js
@@ -0,0 +1,12 @@
+function sortArray (array, input) {
+
+    if (input === 'asc') {
+        array.sort((a,b) => a-b);
+    }
+    else {
+        array.sort((a,b) => b-a);
+    }
+    return array;
+}
+sortArray([14, 7, 17, 6, 8], 'asc');
+sortArray([14, 7, 17, 6, 8], 'desc');
