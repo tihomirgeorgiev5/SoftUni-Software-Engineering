b3da5bb859d8e5c6a7b664e7ff74b5148d87c427
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-06-11 14:37:15 +0200

    added new exercise => nextArticle

--- JS-Advanced/HomeWorks-And-Labs/06.AdvancedFunctions/08. Next Article/script.js
+++ JS-Advanced/HomeWorks-And-Labs/06.AdvancedFunctions/08. Next Article/script.js
@@ -1,3 +1,13 @@
-function getArticleGenerator(articles) {
-    // TODO
+function getArticleGenerator(input) {
+   const articles = input;
+
+   return ()=> {
+    if (articles.length > 0) {
+        const container = document.getElementById('content');
+        const article = document.createElement('article');
+        const currentText = articles.shift();
+        article.innerText = currentText;
+        container.appendChild(article);
+    }
+   }
 }
