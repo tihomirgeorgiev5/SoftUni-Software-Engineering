02c4ff871e0c4a345854a6e1bf837cba30d0d8bf
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-06-12 15:07:37 +0200

    added  new solution of 01.ForumPosts 100/100 Judge

--- JS-Advanced/Exams/ExamPrep/ExamPrep-06.04.2022/Forum Posts_ÉÑßÒÓß¿/app.js
+++ JS-Advanced/Exams/ExamPrep/ExamPrep-06.04.2022/Forum Posts_ÉÑßÒÓß¿/app.js
@@ -10,7 +10,7 @@ function solve() {
     title: document.getElementById('post-title'),
     category: document.getElementById('post-category'),
     content: document.getElementById('post-content'),
-  }
+  };
 
   const review = document.getElementById('review-list');
   const publish = document.getElementById('published-list');
@@ -29,20 +29,12 @@ function solve() {
     //  the program should not do anything.
 
     if (input.title.value != '' && input.content.value != '' && input.category.value != '') {
-      // const article = createElement('article', null, null);
-      // const headElement = createElement('h3', taskField.value, null);
-      // const pDescription = createElement('p', `Description: ${descriptionField.value}`);
-      // const pDueDate = createElement('p', `Due Date: ${dueDateField.value}`, null );
-      // const buttonsDiv = createElement('buttonsDiv', null, 'flex');
-      // const buttonStart = createElement('button', 'Start', 'green');
-      // const buttonFinish = createElement('button', 'Finish', 'orange');
-      // const buttonDelete = createElement('button', 'Delete', 'red');
 
       const li = createElement('li', null, 'rpost');
       const article = createElement('article', null, null);
       const headEl = createElement('h4', input.title.value, null);
-      const pCategory = createElement('p', input.category.value, null);
-      const pContent = createElement('p', input.content.value, null);
+      const pCategory = createElement('p',`Category: ${input.category.value}`, null);
+      const pContent = createElement('p', `Content: ${input.content.value}`, null);
       const editBtn = createElement('button', 'Edit', 'action-btn edit');
       const approveBtn = createElement('button', 'Approve', 'action-btn approve');
 
@@ -77,8 +69,8 @@ function solve() {
   function onEdit(e) {
     const currentArticle = e.target.previousSibling;
     input.title.value = currentArticle.querySelector('h4').textContent;
-    input.category.value = currentArticle.querySelectorAll('p')[0].textContent;
-    input.content.value = currentArticle.querySelectorAll('p')[1].textContent;
+    input.category.value = currentArticle.querySelectorAll('p')[0].textContent.replace('Category: ', '');
+    input.content.value = currentArticle.querySelectorAll('p')[1].textContent.replace('Content: ', '');
 
     currentArticle.parentNode.remove();
   }
