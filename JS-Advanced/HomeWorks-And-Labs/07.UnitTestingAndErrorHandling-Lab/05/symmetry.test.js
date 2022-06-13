b40c09157aebde40c1e0b6c8f64e657107e1177f
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-06-13 18:58:15 +0200

    added mocha and chai

---
+++ JS-Advanced/HomeWorks-And-Labs/07.UnitTestingAndErrorHandling-Lab/05/symmetry.test.js
@@ -0,0 +1,8 @@
+const { expect } = require('chai');
+const { isSymmetric } = require('./symmetry');
+
+describe('Symmetry Checker', () => {
+    it('works', () => {
+        'do nothing';
+    });
+});
