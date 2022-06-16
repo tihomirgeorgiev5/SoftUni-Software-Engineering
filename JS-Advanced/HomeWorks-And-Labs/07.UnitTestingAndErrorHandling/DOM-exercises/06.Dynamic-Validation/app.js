5f775f6d47f00124b7854bb2e390a9acf5272507
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-06-16 17:08:53 +0200

    added new DOM exercise in UnitTestings exercises

--- JS-Advanced/HomeWorks-And-Labs/07.UnitTestingAndErrorHandling/DOM-exercises/06.Dynamic-Validation/app.js
+++ JS-Advanced/HomeWorks-And-Labs/07.UnitTestingAndErrorHandling/DOM-exercises/06.Dynamic-Validation/app.js
@@ -1,3 +1,17 @@
 function validate() {
-    // TODO
+    document.getElementById('email')
+    .addEventListener('change', onChange);
+
+    function onChange(ev) {
+        const pattern = /[a-z]+@[a-z]+\.[a-z]+/gm;
+
+        if (pattern.test(ev.target.value)) {
+            ev.target.classList.remove('error');
+        } else {
+            ev.target.classList.add('error');
+        }
+        
+    }
+
+
 }
