e7745a4212c5569d0a75cfa84ca1d6b96226bdb0
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-06-23 18:18:12 +0200

    solve solution

--- JS-Advanced/Exams/ExamPrep/ExamPrep1-12-02.2021/02.Parking/parking.js
+++ JS-Advanced/Exams/ExamPrep/ExamPrep1-12-02.2021/02.Parking/parking.js
@@ -79,9 +79,11 @@ class Parking {
     addCar(carModel, carNumber) {
         if (this.capacity === 0) {
             throw new Error("Not enough parking space.");
-        }
-        this.vehicles.push({ carModel, carNumber, payed: false });
+        } else {
+            this.vehicles.push({carModel, carNumber, payed: false});
         this.capacity--;
+        }
+        
         return `The ${carModel}, with a registration number ${carNumber}, parked.`;
     }
 
@@ -97,7 +99,33 @@ class Parking {
         this.capacity++;
 
         return `${carNumber} left the parking lot.`;
+    }
 
+    pay(carNumber) {
+        let car = this.vehicles.find(car => car.carNumber === carNumber);
+        if (!car) {
+            throw new Error(`${carNumber} is not in the parking lot.`)
+        }
+        if (car.payed) {
+            throw new Error(`${carNumber}'s driver has already payed his ticket.`)
+        }
+         car.payed = true;
+         return `${carNumber}'s driver successfully payed for his stay.`;
+
+    }
+    getStatistics(carNumber) {
+        if (!carNumber) {
+            return [
+                `The Parking Lot has ${this.capacity} empty spots left.`,
+                this.vehicles
+                .sort((a,b) => a.carModel.localeCompare(b.carModel))
+                .map(car => `${car.carModel} == ${car.carNumber} - ${car.payed ? 'Has payed' : 'Not payed'}`)
+                .join('\n')
+            ].join('\n');
+        }
+        let car = this.vehicles.find(car => car.carNumber === carNumber);
+        return `${car.carModel} == ${car.carNumber} - ${car.payed ? 'Has payed' : 'Not payed'}`;
+        
 
     }
 }
@@ -105,7 +133,7 @@ class Parking {
 const parking = new Parking(12);
 
 console.log(parking.addCar("Volvo t600", "TX3691CA"));
-// console.log(parking.getStatistics());
+console.log(parking.getStatistics());
 
-// console.log(parking.pay("TX3691CA"));
+ console.log(parking.pay("TX3691CA"));
  console.log(parking.removeCar("TX3691CA"));
