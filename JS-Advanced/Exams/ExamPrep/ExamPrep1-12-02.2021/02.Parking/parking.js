05c2fb064770baa295bfb00e58276681c09c66e3
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-06-23 02:27:54 +0200

    beginning task, i wil continued tommorow

--- JS-Advanced/Exams/ExamPrep/ExamPrep1-12-02.2021/02.Parking/parking.js
+++ JS-Advanced/Exams/ExamPrep/ExamPrep1-12-02.2021/02.Parking/parking.js
@@ -1,79 +1,111 @@
+// class Parking {
+//     constructor ( capacity )  {
+//         this.capacity = capacity;
+//         this.vehicles = [];
+//     }
+
+//     addCar( carModel, carNumber ) {
+//         if(this.capacity === 0) {
+//             throw new Error(`Not enough parking space.`)
+//         } else {
+//             this.vehicles.push ({carModel, carNumber, payed: false});
+//             this.capacity --;
+//         }
+
+//         return `The ${carModel}, with a registration number ${carNumber}, parked.`
+//     }
+
+//     removeCar( carNumber ) {
+//         const carIndex = this.vehicles.findIndex(car => car.carNumber === carNumber);
+//         if(carIndex < 0) {
+//             throw new Error (`The car, you're looking for, is not found.`);
+//         }
+
+//         if(!this.vehicles[carIndex].payed) {
+//             throw new Error (`${carNumber} needs to pay before leaving the parking lot.`);
+//         }
+
+//         this.vehicles.splice(carIndex, 1);
+//         this.capacity++;
+
+//         return `${carNumber} left the parking lot.`
+
+
+//     }
+
+//     pay( carNumber ) {
+//         const car = this.vehicles.find(car => car.carNumber === carNumber);
+
+//         if(!car) {
+//             throw new Error (`${carNumber} is not in the parking lot.`);
+//         }
+
+//         if(car.payed) {
+//             throw new Error (`${carNumber}'s driver has already payed his ticket.`);
+//         }
+
+//         car.payed = true;
+
+//         return `${carNumber}'s driver successfully payed for his stay.`;
+
+
+//     }
+
+//     getStatistics(carNumber) {
+//         if(!carNumber) {
+//             return [
+//                 `The Parking Lot has ${this.capacity} empty spots left.`,
+//                 this.vehicles
+//                 .sort((a,b) => a.carModel.localeCompare(b.carModel))
+//                 .map(car => `${car.carModel} == ${car.carNumber} - ${car.payed ? 'Has payed' : 'Not payed'}`)
+//                 .join('\n')
+
+//             ].join('\n');
+//         }
+
+//         let car = this.vehicles.find(car => car.carNumber === carNumber);
+
+//         return `${car.carModel} == ${car.carNumber} - ${car.payed ? 'Has payed' : 'Not payed'}`
+//     }
+
+// }
+
 class Parking {
-    constructor ( capacity )  {
+    constructor(capacity) {
         this.capacity = capacity;
         this.vehicles = [];
     }
 
-    addCar( carModel, carNumber ) {
-        if(this.capacity === 0) {
-            throw new Error(`Not enough parking space.`)
-        } else {
-            this.vehicles.push ({carModel, carNumber, payed: false});
-            this.capacity --;
+    addCar(carModel, carNumber) {
+        if (this.capacity === 0) {
+            throw new Error("Not enough parking space.");
         }
-
-        return `The ${carModel}, with a registration number ${carNumber}, parked.`
+        this.vehicles.push({ carModel, carNumber, payed: false });
+        this.capacity--;
+        return `The ${carModel}, with a registration number ${carNumber}, parked.`;
     }
 
-    removeCar( carNumber ) {
-        const carIndex = this.vehicles.findIndex(car => car.carNumber === carNumber);
-        if(carIndex < 0) {
-            throw new Error (`The car, you're looking for, is not found.`);
+    removeCar(carNumber) {
+        const indexCar = this.vehicles.findIndex(car => car.carNumber === carNumber);
+        if (indexCar < 0) {
+            throw new Error("The car, you're looking for, is not found.");
         }
-
-        if(!this.vehicles[carIndex].payed) {
-            throw new Error (`${carNumber} needs to pay before leaving the parking lot.`);
+        if (!this.vehicles[indexCar].payed) {
+            throw new Error(`${carNumber} needs to pay before leaving the parking lot.`);
         }
-
-        this.vehicles.splice(carIndex, 1);
+        this.vehicles.splice(indexCar, 1);
         this.capacity++;
 
-        return `${carNumber} left the parking lot.`
+        return `${carNumber} left the parking lot.`;
 
 
     }
-
-    pay( carNumber ) {
-        const car = this.vehicles.find(car => car.carNumber === carNumber);
-
-        if(!car) {
-            throw new Error (`${carNumber} is not in the parking lot.`);
-        }
-
-        if(car.payed) {
-            throw new Error (`${carNumber}'s driver has already payed his ticket.`);
-        }
-
-        car.payed = true;
-
-        return `${carNumber}'s driver successfully payed for his stay.`;
-
-
-    }
-
-    getStatistics(carNumber) {
-        if(!carNumber) {
-            return [
-                `The Parking Lot has ${this.capacity} empty spots left.`,
-                this.vehicles
-                .sort((a,b) => a.carModel.localeCompare(b.carModel))
-                .map(car => `${car.carModel} == ${car.carNumber} - ${car.payed ? 'Has payed' : 'Not payed'}`)
-                .join('\n')
-
-            ].join('\n');
-        }
-
-        let car = this.vehicles.find(car => car.carNumber === carNumber);
-
-        return `${car.carModel} == ${car.carNumber} - ${car.payed ? 'Has payed' : 'Not payed'}`
-    }
-
 }
 
 const parking = new Parking(12);
 
 console.log(parking.addCar("Volvo t600", "TX3691CA"));
-console.log(parking.getStatistics());
+// console.log(parking.getStatistics());
 
-console.log(parking.pay("TX3691CA"));
-console.log(parking.removeCar("TX3691CA"));
+// console.log(parking.pay("TX3691CA"));
+ console.log(parking.removeCar("TX3691CA"));
