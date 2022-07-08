5d2a51b3913b3de01fe76986d13363d4556eaea1
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-07-08 17:46:59 +0200

    added second task of AJAX-exercises

--- JS-Application/HomeWorks-And-Labs/02.AsyncProgramming/02.Bus-Schedule/app.js
+++ JS-Application/HomeWorks-And-Labs/02.AsyncProgramming/02.Bus-Schedule/app.js
@@ -1,11 +1,36 @@
-function solve() {
+ function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    const label = document.querySelector('#info span');
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+
+    let stop = {
+        next: 'depot'
+    };
+
+
+
+    async function depart() {
+        // get info for next stop
+        // display next stop
+
+        departBtn.disabled = true;
+        arriveBtn.disabled = false; 
+
+        const url = `http://localhost:3030/jsonstore/bus/schedule/${stop.next}`;
+        const res = await fetch(url);
+
+        stop = await res.json();
+
+        label.textContent = `Next stop ${stop.name}`;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+
+        label.textContent = `Arriving at ${stop.name}`
+        departBtn.disabled = false;
+        arriveBtn.disabled = true; 
+        
     }
 
     return {
