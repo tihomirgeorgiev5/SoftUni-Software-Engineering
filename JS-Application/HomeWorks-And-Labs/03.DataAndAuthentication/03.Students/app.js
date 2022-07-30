3f852c2c4cfec99ab9735c671b099cd2090ced29
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-07-30 19:29:54 +0200

    added Students task in Data exercises

--- JS-Application/HomeWorks-And-Labs/03.DataAndAuthentication/03.Students/app.js
+++ JS-Application/HomeWorks-And-Labs/03.DataAndAuthentication/03.Students/app.js
@@ -1,1 +1,89 @@
-console.log('TODO...');
+async function students() {
+    const url = 'http://localhost:3030/jsonstore/collections/students';
+    const table = document.querySelector('#results tbody');
+    const response = await fetch(url);
+    const data = await response.json();
+
+    Object.values(data).forEach(s => {
+        const firstName = s.firstName;
+        const lastName = s.lastName;
+        const facultyNumber = s.facultyNumber;
+        const grade = Number(s.grade);
+
+        const tr = document.createElement('tr');
+
+        const firstNameCell = tr.insertCell(0);
+        firstNameCell.innerText = firstName;
+
+        const lastNameCell = tr.insertCell(1);
+        lastNameCell.innerText = lastName;
+
+        const facultyNumCell = tr.insertCell(2);
+        facultyNumCell.innerText = facultyNumber;
+
+        const gradeCell = tr.insertCell(3);
+        gradeCell.innerText = grade;
+
+
+        table.appendChild(tr);
+
+    });
+
+    const submitBtn = document.getElementById('submit');
+    submitBtn.addEventListener('click', onSubmit);
+
+    async function onSubmit(ev) {
+        ev.preventDefault();
+
+        const firstNameInput = document.querySelector('[name="firstName"]');
+        const lastNameInput = document.querySelector('[name="lastName"]');
+        const facultyNumInput = document.querySelector('[name="facultyNumber"]');
+        const gradeInput = document.querySelector('[name="grade"]');
+
+        if (isNaN(facultyNumInput.value) || isNaN(gradeInput.value)) {
+            return alert('Wrong input data!');
+        }
+
+        if (firstNameInput.value !== '' &&
+            lastNameInput.value !== '' &&
+            facultyNumInput.value !== '' &&
+            gradeInput.value !== '') {
+            const res = await fetch(url, {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    firstName: firstNameInput.value,
+                    lastName: lastNameInput.value,
+                    facultyNumber: Number(facultyNumInput.value),
+                    grade: Number(gradeInput.value)
+                })
+
+            });
+
+            const tr = document.createElement('tr');
+            const firstNameCell = tr.insertCell(0);
+            firstNameCell.innerText = firstNameInput.value;
+
+            const lastNameCell = tr.insertCell(1);
+            lastNameCell.innerText = lastNameInput.value;
+
+            const facultyNumCell = tr.insertCell(2);
+            facultyNumCell.innerText = facultyNumInput.value;
+
+            const gradeCell = tr.insertCell(3);
+            gradeCell.innerText = gradeInput.value;
+            table.appendChild(tr);
+        }
+
+        firstNameInput.value = '';
+        lastNameInput.value = '';
+        facultyNumInput.value = '';
+        gradeInput.value = '';
+
+    }
+
+}
+
+students();
