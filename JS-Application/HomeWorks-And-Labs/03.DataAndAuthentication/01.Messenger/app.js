375895f8db92801cb6dcd25aec983e1f111fda16
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-07-30 11:40:29 +0200

    added first exercise Messager from Data exercises

--- JS-Application/HomeWorks-And-Labs/03.DataAndAuthentication/01.Messenger/app.js
+++ JS-Application/HomeWorks-And-Labs/03.DataAndAuthentication/01.Messenger/app.js
@@ -1,5 +1,49 @@
+const url = 'http://localhost:3030/jsonstore/messenger';
+const messages = document.getElementById('messages');
 function attachEvents() {
-    console.log('TODO...');
+
+    document.getElementById('submit').addEventListener('click', onSubmit);
+    document.getElementById('refresh').addEventListener('click', onRefresh);
+
+}
+
+async function onSubmit() {
+    const [author, content] = [document.querySelector('[name="author"]'),
+    document.querySelector('[name="content"]')];
+
+    if (author.value != '' || content.value != '') {
+        await request(url, {
+            author: author.value,
+            content: content.value
+        });
+        author.value = '';
+        content.value = '';
+        
+    }
+
+}
+
+async function onRefresh() {
+    const res = await fetch(url);
+    const data = await res.json();
+    messages.value = Object.values(data).map(({ author, content }) => `${author}: ${content}`).join('\n');
+}
+
+async function request(url, option) {
+
+    if (option) {
+        option = {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(option)
+        };
+    }
+    const response = await fetch(url, option);
+
+    return response.json();
+
 }
 
 attachEvents();
