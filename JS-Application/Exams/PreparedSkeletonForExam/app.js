9c36104825cbc6e1a2944f81a3814ccb105c43a1
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-08-12 00:34:20 +0200

    added files in ExamSkeleton

---
+++ JS-Application/Exams/PreparedSkeletonForExam/app.js
@@ -0,0 +1,26 @@
+import {page, render} from './lib.js';
+import { homeView } from './views/home.js';
+
+const main = document.querySelector('main');
+
+page(decorateContext);
+page('/', homeView);
+page('/memes', () => console.log('catalog'));
+page('/memes/:id', () => console.log('details'));
+page('/edit/:id', () => console.log('edit'));
+page('/login', () => console.log('login'));
+page('/register', () => console.log('register'));
+page('/create', () => console.log('create'));
+page('/profile', () => console.log('profile'));
+
+page.start();
+
+function decorateContext(ctx, next) {
+    ctx.render = renderMain;
+
+    next();
+}
+
+function renderMain(templateResult) {
+    render(templateResult, main);
+}
