a400a2f1e489d1f2a5e2c60ffaceac3aeb8ce355
Tihomir Georgiev <georgiev.tm@abv.bg>
2022-08-14 09:30:15 +0200

    update examPrepSkeleton folder

--- JS-Application/Exams/PreparedSkeletonForExam2/notify.js
+++
@@ -1,9 +0,0 @@
-const container = document.getElementById('errorBox');
-const span = container.querySelector('span');
-
-export function notify(message) {
-    span.textContent = message;
-    container.style.display = 'block';
-
-    setTimeout(() => container.style.display = 'none', 3000) 
-}
