5a50588dd091316ba7c2b08ede1f5b1411ac0442
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-02-19 11:16:08 +0100

    added 07.Projects_Creation

---
+++ Python-Programming-Basics/HomeWorks-And-Labs/01.FirstSteps_In_Coding-lab/07.Projects_Creation.py
@@ -0,0 +1,5 @@
+name = input()
+count_projects = int(input())
+total_projects_count = count_projects * int(3)
+
+print(f'The architect {name} will need {total_projects_count} hours to complete {count_projects} project/s.')
