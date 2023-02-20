d609dc0238f86da07e4c040375923f882eb8eac3
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-02-20 01:02:09 +0100

    added 07.Projects_Creation

---
+++ Python-Programming-Basics/HomeWorks-And-Labs/01.FirstSteps_In_Coding-lab/08.Pet_Shop.py
@@ -0,0 +1,9 @@
+dog_food_qty = int(input())
+cat_food_qty = int(input())
+total_dog_food_price = 2.50 * dog_food_qty
+total_cat_food_price = 4 * cat_food_qty
+
+total_price = total_dog_food_price + total_cat_food_price
+
+print(f'"{total_price} lv."')
+
