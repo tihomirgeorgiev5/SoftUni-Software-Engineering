0e07d57fad29529ef221514e83ba14c34a609f63
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-02-25 11:20:14 +0100

    Added 08. Basketball Equipment

---
+++ Python-Programming-Basics/HomeWorks-And-Labs/01.FirstSteps_In_Coding/09.Fish_Tank.py
@@ -0,0 +1,10 @@
+length = int(input())
+width = int(input())
+height = int(input())
+percent = float(input())
+
+volume = length * width * height
+volume_in_liters = volume * 0.001
+
+needed_volume = volume_in_liters - (volume_in_liters * (percent * 0.01))
+print(needed_volume)
