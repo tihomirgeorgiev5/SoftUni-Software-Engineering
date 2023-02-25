b99adced6edb3a1f885c9a837f9517c2b2298f99
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-02-25 11:23:40 +0100

    Added 09. Fish Tank

--- Python-Programming-Basics/HomeWorks-And-Labs/01.FirstSteps_In_Coding/09.Fish_Tank.py
+++ Python-Programming-Basics/HomeWorks-And-Labs/01.FirstSteps_In_Coding/09.Fish_Tank.py
@@ -5,6 +5,5 @@ percent = float(input())
 
 volume = length * width * height
 volume_in_liters = volume * 0.001
-
 needed_volume = volume_in_liters - (volume_in_liters * (percent * 0.01))
 print(needed_volume)
