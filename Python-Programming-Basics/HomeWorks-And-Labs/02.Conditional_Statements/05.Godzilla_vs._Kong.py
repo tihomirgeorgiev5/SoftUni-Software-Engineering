4582f3c01ee8d4d8586be419ebfa0ea12049ce25
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-03-02 20:41:12 +0100

    Added 05. Godzilla vs. Kong (90/100_in_Judge)

---
+++ Python-Programming-Basics/HomeWorks-And-Labs/02.Conditional_Statements/05.Godzilla_vs._Kong.py
@@ -0,0 +1,20 @@
+budget = float(input())
+actors_count = int(input())
+actors_clothes_price = float(input())
+
+decor = budget * 0.10
+total_clothes_price = actors_count * actors_clothes_price
+
+if actors_count >= 150:
+    total_clothes_price*= 0.90
+
+total_price_for_movie = total_clothes_price + decor
+
+left_money = abs(total_price_for_movie - budget)
+
+if left_money >= budget:
+    print("Not enough money!")
+    print(f"Wingard needs {left_money:.2f} leva more.")
+else:
+    print("Action!")
+    print(f"Wingard starts filming with {left_money:.2f} leva left.")
