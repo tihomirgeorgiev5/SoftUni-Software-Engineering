1b7dea46cb078b402c56d4c30d40828fba643b7c
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-03-07 20:41:26 +0100

    Added 08. Lunch Break(100/100 in Judge)

--- Python-Programming-Basics/HomeWorks-And-Labs/02.Conditional_Statements/08.Lunch_Break.py
+++ Python-Programming-Basics/HomeWorks-And-Labs/02.Conditional_Statements/08.Lunch_Break.py
@@ -8,9 +8,11 @@ lunch_time = pause_length / 8
 relax_time = pause_length / 4
 
 total_time_pause_before_watching = pause_length - lunch_time - relax_time
-left_time = math.ceil(episode_length - total_time_pause_before_watching)
+total_time = abs(episode_length - total_time_pause_before_watching)
+#total_time = math.ceil(episode_length - total_time_pause_before_watching)
 
-if episode_length <= total_time_pause_before_watching:
-    print(f'You have enough time to watch {serial_name} and left with {left_time} minutes free time.')
+
+if total_time_pause_before_watching >= episode_length:
+    print(f'You have enough time to watch {serial_name} and left with {math.ceil(total_time)} minutes free time.')
 else:
-    print(f"You don't have enough time to watch {serial_name}, you need {left_time} more minutes.")
+    print(f"You don't have enough time to watch {serial_name}, you need {math.ceil(total_time)} more minutes.")
