74d1a0cf1e1dd07c52160eaba75461dc835dac82
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-03-22 20:26:02 +0100

    added 02. Weekend or Working Day - new Variant of Cond_Stat_Adv-lab

---
+++ Python-Programming-Basics/HomeWorks-And-Labs/03.Conditional_Statements_Advanced-lab/02.Weekend_Or_Working_Day_Next_Variante.py
@@ -0,0 +1,12 @@
+
+day_of_week = input();
+result = ''
+
+if day_of_week == 'Saturday' or day_of_week == 'Sunday':
+    result = 'Weekend'
+elif day_of_week == 'Monday' or day_of_week == 'Tuesday' or day_of_week == 'Wednesday' or day_of_week == 'Thursday' or day_of_week == 'Friday':
+    result = 'Working day'
+else:
+    result = 'Error'
+
+print(result)
