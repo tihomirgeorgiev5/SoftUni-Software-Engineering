1cd913d84aefe520aa9bdd320476d91a8228dc5e
Tihomir Georgiev <georgiev.tm@abv.bg>
2023-03-25 15:43:04 +0100

    added 07.Working Hours of Cond_Stat_Adv-lab

---
+++ Python-Programming-Basics/HomeWorks-And-Labs/03.Conditional_Statements_Advanced-lab/07.Working_Hours.py
@@ -0,0 +1,18 @@
+
+hour = int(input())
+day_of_week = input()
+
+
+if day_of_week == 'Monday' or \
+    day_of_week == 'Tuesday' or \
+    day_of_week == 'Wednesday' or \
+    day_of_week == 'Thursday' or \
+    day_of_week == 'Friday' or \
+    day_of_week == 'Saturday':
+    if 10 <= hour <= 18:
+        print('open')
+    else:
+        print('closed')
+else:
+    print('closed')
+
