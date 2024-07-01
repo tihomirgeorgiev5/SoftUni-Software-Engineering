3baa072faff7cc520608c5bf196234661ee2f4f8
Tihomir Georgiev <georgiev.tm@abv.bg>
2024-07-01 20:54:32 +0200

    publish-subscribe messages training

--- JS-BackEnd/02_Node_js_StreamsAndUtilities_demo/pubsub/demo.js
+++ JS-BackEnd/02_Node_js_StreamsAndUtilities_demo/pubsub/demo.js
@@ -1,3 +1,7 @@
 const eventBus = require('./eventBus');
 
-eventBus.publish();
+eventBus.subscribe('say-hello', () => console.log('event say-hello executed'));
+eventBus.subscribe('say-hello', () => console.log('event say-hello executed second time'));
+eventBus.subscribe('say-bye', () => console.log('event say-bye executed'));
+
+eventBus.publish('say-bye');
--- JS-BackEnd/02_Node_js_StreamsAndUtilities_demo/pubsub/eventBus.js
+++ JS-BackEnd/02_Node_js_StreamsAndUtilities_demo/pubsub/eventBus.js
@@ -1,9 +1,12 @@
 const subscribers = {};
-
+ 
 exports.subscribe = (eventType, callback) => {
-    console.log('subscribe');
+    if (!subscribers[eventType]) {
+        subscribers[eventType]=[];
+    }
+    subscribers[eventType].push(callback);
 };
 
 exports.publish = (eventType) => {
-    console.log('publish');
-}
+    subscribers[eventType].forEach(x => x ());
+};
