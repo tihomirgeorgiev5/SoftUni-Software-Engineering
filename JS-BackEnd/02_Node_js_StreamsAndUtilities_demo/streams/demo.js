befaf4692620f7ef7c0975e3a957aff6ca20cb9c
Tihomir Georgiev <georgiev.tm@abv.bg>
2024-07-05 22:34:16 +0200

    create a file with writeStream

--- JS-BackEnd/02_Node_js_StreamsAndUtilities_demo/streams/demo.js
+++ JS-BackEnd/02_Node_js_StreamsAndUtilities_demo/streams/demo.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const readStream = fs.createReadStream('./streams/largeFile.txt', { encoding: 'utf-8'});
+const writeStream = fs.createWriteStream('./streams/copyFile.txt', { encoding: 'utf-8'});
 
 readStream.on('data', (chunk) => {
     console.log(chunk);
@@ -9,3 +10,7 @@ readStream.on('data', (chunk) => {
 readStream.on('end', () => {
     console.log('Finished');
 })
+
+writeStream.write('Hello World');
+writeStream.write('\n');
+writeStream.write('Hello World 2');
